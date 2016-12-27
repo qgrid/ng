13afe87eb7ab9f9362814efa27f2d180415df5e7
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2016-12-27 18:26:17 +0300

    Added value as formatter

--- demo/sandbox/index.js
+++ demo/sandbox/index.js
@@ -6,10 +6,74 @@ export default function Controller($http) {
 
 	this.rows = [];
 	this.columns = [
-		{key: 'gender', title: 'Gender'},
-		{key: 'birthday', title: 'Birthday'},
-		{key: 'region', title: 'Region'},
-		{key: 'memberSince', title: 'Member Since'}
+		{
+			key: 'name.last',
+			title: 'Last Name',
+			value: function (item) {
+				return item.name.last;
+			}
+		},
+		{
+			key: 'name.first',
+			title: 'First Name',
+			value: function (item) {
+				return item.name.first;
+			}
+		},
+		{
+			key: 'gender',
+			title: 'Gender'
+		},
+		{
+			key: 'birthday',
+			title: 'Birthday'
+		},
+		{
+			key: 'contact.address.zip',
+			title: 'Zip',
+			value: function (item) {
+				return item.contact.address.zip;
+			}
+		},
+		{
+			key: 'contact.address.state',
+			title: 'State',
+			value: function (item) {
+				return item.contact.address.state;
+			}
+		},
+		{
+			key: 'contact.address.city',
+			title: 'City',
+			value: function (item) {
+				return item.contact.address.city;
+			}
+		},
+		{
+			key: 'contact.phone.primary',
+			title: 'Primary Phone',
+			value: function (item) {
+				return item.contact.phone[0];
+			}
+		},
+		{
+			key: 'contact.email.primary',
+			title: 'Primary Email',
+			value: function (item) {
+				return item.contact.email[0];
+			}
+		},
+		{
+			key: 'likes',
+			title: 'Likes',
+			value: function (item) {
+				return item.likes.join(', ');
+			}
+		},
+		{
+			key: 'memberSince',
+			title: 'Member Since'
+		}
 	];
 
 	$http.get('data/people/100.json')
