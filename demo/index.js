82edae7d82e63146db1b1d36d6a56a111ca1e679
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2016-12-29 16:08:46 +0300

    add separate setup file for routing configuration

--- demo/index.js
+++ demo/index.js
@@ -8,6 +8,8 @@ import {} from 'angular-material';
 import qgrid from '../src/index';
 import theme from '../src/themes/default/index';
 import App from './components/app/app';
+import setup from './setup';
+
 require('./index.scss');
 require('angular-material/angular-material.css');
 
@@ -19,45 +21,11 @@ const dependencies = [
 	qgrid,
 	theme
 ];
-
 const pages = require('./pages/pages.json');
-const controllers = pages.reduce((memo, p) => {
-	// TODO: use es6 imports
-	memo[p.path] = require('./pages/' + p.path + '/index').default;
-	return memo;
-}, {});
+const Setup = setup(pages);
 
 export default angular.module('demo', dependencies)
 	.config(Setup)
 	.controller('Demo.App.Controller', App)
 	.constant('Demo.PAGES', pages)
 	.name;
-
-
-Setup.$inject = ['$routeProvider', '$locationProvider', 'Demo.PAGES'];
-function Setup($routeProvider, $locationProvider, pages) {
-	pages.forEach(page => {
-		$routeProvider
-			.when('/' + page.path, {
-				templateUrl: `pages/${page.path}/index.html`,
-				controller: controllers[page.path],
-				controllerAs: '$ctrl'
-			});
-	});
-
-	$routeProvider
-		.when('/', {
-			templateUrl: 'pages/home/index.html',
-			controllers: controllers.home,
-			controllerAs: '$ctrl'
-		})
-		.otherwise({
-			redirectTo: '/'
-		});
-
-
-	// TODO: setup history api for the webpack-dev-server
-	$locationProvider
-		.html5Mode(false)
-		.hashPrefix('!');
-}
