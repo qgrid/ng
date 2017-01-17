69133ecf3cbcebc183a57cbbd99983e9c02b3e87
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-01-17 14:22:16 +0300

    Remove unused files

--- components/app/app.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-Controller.$inject = ['Demo.PAGES'];
-export default function Controller(pages) {
-	this.pages = pages;
-}
--- pages/home/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-Controller.$inject = [];
-export default function Controller() {
-}
