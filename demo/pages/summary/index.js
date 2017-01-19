cd1e4106813b7e3a1339868645592514c7d347bd
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-01-19 23:05:17 +0300

    better example of summary

--- demo/pages/summary/index.js
+++ demo/pages/summary/index.js
@@ -59,9 +59,14 @@ export default function Controller($http) {
 		}
 	];
 
-	this.getCountZip = function (rows, getter) {
-		return rows.map(getter).length;
+	this.getCountEvenZip = function (rows, getter) {
+		return rows.map(getter).filter(z => z % 2).length;
 	};
+
+	this.getCountGender = function (rows, gender) {
+		return rows.filter(r => r.gender === gender).length;
+	};
+
 
 	$http.get('data/people/100.json')
 		.then(function (response) {
