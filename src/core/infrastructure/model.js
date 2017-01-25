5124f3217dbf863eff63ec3a7193da01db25269e
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-01-25 13:47:02 +0300

    fixed event scope

--- src/core/infrastructure/model.js
+++ src/core/infrastructure/model.js
@@ -43,13 +43,13 @@ export default class Model {
 								oldValue: oldValue
 							};
 						}
+					}
 
-						if (hasChanges) {
-							event.emit({
-								state: model,
-								changes: changes
-							});
-						}
+					if (hasChanges) {
+						event.emit({
+							state: model,
+							changes: changes
+						});
 					}
 				}
 
