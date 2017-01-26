83d9351333792b281511a4bb2a0db60c65853914
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-01-27 02:03:26 +0300

    Rename pager model to pagination

---
+++ src/core/pagination/pagination.model.js
@@ -0,0 +1,9 @@
+import Resource from '../resource/resource';
+
+export default class PaginationModel {
+	constructor() {
+		this.resource = new Resource();
+		this.size = 50;
+		this.sizeList = [5, 10, 20, 30, 40, 50];
+	}
+}
