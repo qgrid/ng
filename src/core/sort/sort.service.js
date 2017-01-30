908183b3a7857add3f8b1c544ee997e4723e5abe
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-01-30 16:21:40 +0300

    Show arrows

---
+++ src/core/sort/sort.service.js
@@ -0,0 +1,35 @@
+import AppError from 'core/infrastructure/error';
+
+export function key(entry) {
+	return Object.keys(entry)[0];
+}
+
+export function index(by, entryKey) {
+	return by.map(key).findIndex(k => k === entryKey);
+}
+
+export function direction(entry) {
+	const key = key(entry);
+	if (!key) {
+		throw new AppError(
+			'sort.service',
+			`Sort key is not defined in "${entry}"`);
+	}
+
+	return entry[key];
+}
+
+export function map(by) {
+	return by.reduce((memo, entry) => {
+		const entryKey = key(entry);
+		if (!entryKey) {
+			throw new AppError(
+				'sort.service',
+				`Sort key is not defined in "${by}"`);
+		}
+
+		memo[entryKey] = entry[entryKey];
+		return memo;
+	}, {});
+}
+
