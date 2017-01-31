dc550d6a2f52e998cc078219976da50c19e90693
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-01-31 18:49:18 +0300

    Generate foot rows;

--- src/core/column/column.service.js
+++ src/core/column/column.service.js
@@ -4,3 +4,9 @@ export function map(columns) {
 		return memo;
 	}, {});
 }
+
+export function view(columns, model) {
+	const groupBy = new Set(model.group().by);
+	const pivotBy = new Set(model.pivot().by);
+	return columns.filter(c => !groupBy.has(c.model.key) && !pivotBy.has(c.model.key));
+}
--- src/core/view/view.foot.js
+++ src/core/view/view.foot.js
@@ -1,7 +1,18 @@
 import View from './view';
+import {view as columnView} from 'core/column/column.service';
 
 export default class FootView extends View {
 	constructor(model) {
 		super(model);
+
+		this.rows = [];
+		model.viewChanged.watch(() => this.invalidate(model));
+	}
+
+	invalidate(model) {
+		const columns = columnView(model.view().columns, model);
+		const row = {columns: columns};
+		const length = 1;
+		this.rows = new Array(length).fill(row);
 	}
 }
