64374839742fdb1f687de459b733fd5f7857f7fb
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-02-01 20:20:47 +0300

    Aggregate values on pivot

--- src/core/group/group.build.js
+++ src/core/group/group.build.js
@@ -21,40 +21,44 @@ export default function GroupBuilder(model) {
 		const result = [];
 		for (let i = 0, nodeLength = nodes.length; i < nodeLength; i++) {
 			const node = nodes[i];
-			// const key = groupBy[Math.min(node.level, groupByLength - 1)];
-			// const column = columnMap[key];
-			// if (!column) {
-			// 	throw new AppError(
-			// 		'view.pivot',
-			// 		`Invalid key "${key}"`);
-			// }
-			//
-			// const aggregation = column.aggregation || 'count';
-			// if (!Aggregation.hasOwnProperty(aggregation)) {
-			// 	throw new AppError(
-			// 		'view.pivot',
-			// 		`Aggregation ${aggregation} is not registered`);
-			// }
+			const key = groupBy[Math.min(node.level, groupByLength - 1)];
+			const column = columnMap[key];
+			if (!column) {
+				throw new AppError(
+					'view.pivot',
+					`Invalid key "${key}"`);
+			}
 
-			// const getValue = valueFactory(column);
-			// const groupRows = node.rows.map(i => rows[i]);
-			// const aggRow = Aggregation[aggregation](groupRows, getValue);
-			// result.push(aggRow);
+			const aggregation = column.aggregation || 'count';
+			if (!Aggregation.hasOwnProperty(aggregation)) {
+				throw new AppError(
+					'view.pivot',
+					`Aggregation ${aggregation} is not registered`);
+			}
+
+			const getValue = valueFactory(column);
+			const aggregate = Aggregation[aggregation];
 
 			const aggRow = new Array(pivotRowLength);
 			for (let j = 0, rowLength = node.rows.length; j < rowLength; j++) {
 				const rowIndex = node.rows[j];
 				const pivotRow = pivotRows[rowIndex];
+				const row = rows[rowIndex];
 				for (let k = 0; k < pivotRowLength; k++) {
 					if (pivotRow[k]) {
-						const value = aggRow[k];
-						aggRow[k] = value ? value + 1 : 1; // apply aggregation
+						let value = aggRow[k];
+						if (!value) {
+							value = [];
+							aggRow[k] = value;
+						}
+						value.push(row);
 					}
 				}
 			}
-			result.push(aggRow);
+
+			result.push(aggRow.map(rs => aggregate(rs, getValue)));
 		}
 
 		return result;
-	}
+	};
 }
