ebbe6d1db2ab370ae4da1f5718f6b922afca2886
stepan.sichkar <stepan.sichkar@arcadia.spb.ru>
2017-02-01 16:54:56 +0300

    aggregation refactoring

--- src/core/services/aggregation.js
+++ src/core/services/aggregation.js
@@ -1,5 +1,3 @@
-import * as _ from './utility';
-
 export default class Aggregation {
 	constructor() {
 	}
@@ -25,7 +23,18 @@ export default class Aggregation {
 			return null;
 		}
 
-		return getValue(_.maxBy(rows, d => getValue(d)));
+		let length = rows.length,
+			i = 0,
+			max = getValue(rows[i++]);
+
+		while (i < length) {
+			if (getValue(rows[i]) > max) {
+				max = getValue(rows[i]);
+			}
+			i++;
+		}
+
+		return max;
 	}
 
 	static min(rows, getValue) {
@@ -33,30 +42,85 @@ export default class Aggregation {
 			return null;
 		}
 
-		return getValue(_.minBy(rows, d => getValue(d)));
+		let length = rows.length,
+			i = 0,
+			min = getValue(rows[i++]);
+
+		while (i < length) {
+			if (getValue(rows[i]) < min) {
+				min = getValue(rows[i]);
+			}
+			i++;
+		}
+
+		return min;
 	}
 
 	static minMax(rows, getValue) {
-		return [this.min(rows, getValue), this.max(rows, getValue)];
+		if (!rows.length) {
+			return null;
+		}
+
+		let length = rows.length,
+			i = 0,
+			min = getValue(rows[i++]),
+			max = min;
+
+		while (i < length) {
+			if (getValue(rows[i]) < min) {
+				min = getValue(rows[i]);
+			}
+
+			if (getValue(rows[i]) > max) {
+				max = getValue(rows[i]);
+			}
+
+			i++;
+		}
+
+		return [min, max];
 	}
 
 	static avg(rows, getValue) {
-		const length = rows.length;
-		return rows.reduce(function (reduced, d) {
-			return reduced + getValue(d) / length;
-		}, 0);
+		if (!rows.length) {
+			return null;
+		}
+
+		return this.sum(rows, getValue) / rows.length;
 	}
 
 	static sum(rows, getValue) {
-		return rows.reduce(function (reduced, d) {
-			return reduced + Number(getValue(d));
-		}, 0);
+		if (!rows.length) {
+			return null;
+		}
+
+		let length = rows.length,
+			i = 0,
+			sum = 0;
+
+		while (i < length) {
+			sum += Number(getValue(rows[i]));
+			i++;
+		}
+
+		return sum;
 	}
 
 	static join(rows, getValue) {
-		return rows.reduce(function (reduced, d) {
-			return reduced + getValue(d).toString();
-		}, '');
+		if (!rows.length) {
+			return null;
+		}
+
+		let length = rows.length,
+			i = 0,
+			join = getValue(rows[i++]);
+
+		while (i < length) {
+			join += '' + getValue(rows[i]);
+			i++;
+		}
+
+		return join;
 	}
 
 	static count(rows) {
