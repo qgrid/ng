6d2adfe7526a3bfa8f13bdfc33fa16e90aaf2321
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-02-03 00:00:46 +0300

    Add column modules

--- src/core/column/column.factory.js
+++ src/core/column/column.factory.js
@@ -1,5 +1,10 @@
 import AppError from 'core/infrastructure/error';
 import TextColumn from './column.text';
+import NumberColumn from './column.number';
+import BoolColumn from './column.bool';
+import DateColumn from './column.date';
+import ArrayColumn from './column.array';
+import EmailColumn from './column.email';
 import SelectColumn from './column.select';
 import GroupColumn from './column.group';
 import PivotColumn from './column.pivot';
@@ -8,6 +13,16 @@ export default function (type, model = null) {
 	switch (type) {
 		case 'text':
 			return new TextColumn(model || TextColumn.model());
+		case 'number':
+			return new NumberColumn(model || NumberColumn.model());
+		case 'bool':
+			return new BoolColumn(model || BoolColumn.model());
+		case 'date':
+			return new DateColumn(model || DateColumn.model());
+		case 'array':
+			return new ArrayColumn(model || ArrayColumn.model());
+		case 'email':
+			return new EmailColumn(model || EmailColumn.model());
 		case 'select':
 			return new SelectColumn(model || SelectColumn.model());
 		case 'group':
