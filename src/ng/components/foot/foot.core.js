888836d2df64cd8a81cab04be409672e50d3bf36
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-02-10 20:58:02 +0300

    sync foot scroll

--- src/ng/components/foot/foot.core.js
+++ src/ng/components/foot/foot.core.js
@@ -2,18 +2,24 @@ import Directive from 'ng/directives/directive';
 import {VIEW_CORE_NAME, FOOT_CORE_NAME} from 'src/definition';
 
 class FootCore extends Directive(FOOT_CORE_NAME, {view: `^^${VIEW_CORE_NAME}`}) {
-	constructor($scope) {
+	constructor($scope, $element) {
 		super();
 
 		this.$scope = $scope;
+		this.element = $element[0];
 		Object.defineProperty($scope, '$view', {get: () => this.view});
 	}
 
 	onInit(){
+		this.view.model.layoutChanged.watch(e => {
+			if (e.changes.hasOwnProperty('scroll')) {
+				this.element.parentNode.parentNode.scrollLeft = e.state.scroll.left;
+			}
+		});
 	}
 }
 
-FootCore.$inject = ['$scope'];
+FootCore.$inject = ['$scope', '$element'];
 
 export default {
 	restrict: 'A',
