42f74c12fcc541f06f8d8f720eeab05fe6ce95c6
Ekaterina Tokareva <etokareva1990@gmail.com>
2017-02-15 15:55:19 +0300

    row single/multiple highlight

--- src/core/selection/selection.view.js
+++ src/core/selection/selection.view.js
@@ -2,13 +2,14 @@ import View from '../view/view';
 import Command from 'core/infrastructure/command';
 import behaviorFactory from './selection.factory';
 import {isUndefined} from 'core/services/utility';
+import {GRID_PREFIX} from 'core/definition';
 
 export default class SelectionView extends View {
-	constructor(model) {
+	constructor(model, markup) {
 		super(model);
 
+		this.markup = markup;
 		this.behavior = behaviorFactory(model);
-
 		this.toggle = new Command({
 			execute: (item, state) => {
 				if (isUndefined(item)) {
@@ -28,7 +29,6 @@ export default class SelectionView extends View {
 		});
 
 		model.viewChanged.watch(() => this.behavior = behaviorFactory(model));
-
 		model.selectionChanged.watch(e => {
 			if (e.tag.source !== 'toggle' && e.changes.hasOwnProperty('items')) {
 				this.behavior.select(e.state.items, true);
@@ -37,8 +37,27 @@ export default class SelectionView extends View {
 			if (e.changes.hasOwnProperty('unit') ||
 				e.changes.hasOwnProperty('mode')) {
 				this.behavior = behaviorFactory(model);
+				e.state.items.forEach((item) => this.highlight(item));
+			}
+			if (e.changes.hasOwnProperty('items') && e.state.unit === 'row') {
+				const values = e.changes.items.newValue.concat(e.changes.items.oldValue);
+				values.forEach((item) => this.highlight(item));
 			}
 		});
+	}
+
+	highlight(item) {
+		const index = this.model.view().rows.indexOf(item);
+		const body = this.markup.body;
+		if (body && body.rows[index]) {
+			for (let cell of body.rows[index].cells) {
+				if (this.state(item)) {
+					cell.classList.add(`${GRID_PREFIX}-selected`);
+				} else {
+					cell.classList.remove(`${GRID_PREFIX}-selected`);
+				}
+			}
+		}
 	}
 
 	state(item) {
