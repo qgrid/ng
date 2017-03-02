27280c6514ab3a8ee0ee9cdff9c53f2016419668
Ekaterina Tokareva <etokareva1990@gmail.com>
2017-03-02 16:13:37 +0300

    deny navigation change while edit mode

--- src/core/highlight/behaviors/cell.highlight.js
+++ src/core/highlight/behaviors/cell.highlight.js
@@ -1,35 +1,16 @@
 import Behavior from './highlight.behavior';
-import {GRID_PREFIX} from 'core/definition';
 
 export default class CellHighlight extends Behavior {
 	constructor(model, markup) {
 		super(model, markup);
 	}
 
-	applyCore(items) {
-		const body = this.markup.body;
-
-		for (let item of items) {
-			const row = this.model.view().rows.indexOf(item);
-			this.state(body.rows[row].cells[this.model.navigation().column], true);
-		}
+	applyCore() {
 	}
 
-	clearCore(items) {
-		const body = this.markup.body;
-		for (let row = 0; row++; row < body.rows.length) {
-			for (let column of row.cells) {
-				this.state(body.rows[row.rowIndex].cells[column.cellIndex], false);
-			}
-		}
+	clearCore() {
 	}
 
-	state(item, state) {
-		if (state) {
-			item.classList.add(`${GRID_PREFIX}-selected`);
-		}
-		else {
-			item.classList.remove(`${GRID_PREFIX}-selected`);
-		}
+	state() {
 	}
 }
