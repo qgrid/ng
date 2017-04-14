e1ce93cf090fa100a4356362686852b75d863644
etokareva <etokareva1990@gmail.com>
2017-04-14 12:35:30 +0300

    test on empty

--- src/ng/services/dom/table.spec.js
+++ src/ng/services/dom/table.spec.js
@@ -1,4 +1,7 @@
 import Table from './table';
+import Cell from './cell';
+import Row from './row';
+import Column from './column';
 import angular from 'angular'; //necessary to test CellDom model property
 
 const myWindow = window.open("", "MsgWindow", "width=100,height=50");
@@ -32,18 +35,18 @@ describe('Table', () => {
 		it('column', () => {
 			const column = table.body.column();
 			const cells = column.cells();
-			expect(column).to.be.an.instanceOf(Object).and.to.have.property('cells');
+			expect(column).to.deep.equal(Column.empty);
 			expect(cells).to.be.an.instanceOf(Array).with.lengthOf(0);
 		});
 		it('row', () => {
 			const row = table.body.row();
 			const cells = row.cells();
-			expect(row).to.be.an.instanceOf(Object).and.to.have.property('cells');
+			expect(row).to.deep.equal(Row.empty);
 			expect(cells).to.be.an.instanceOf(Array).with.lengthOf(0);
 		});
 		it('cell', () => {
 			tableProps.forEach((prop) => {
-				expect(table[prop].cell(0, 0)).to.be.an.instanceOf(Object);
+				expect(table[prop].cell(0, 0)).to.be.deep.equal(Cell.empty);
 			});
 		});
 	});
