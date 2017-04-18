02dbdb057d2e9492fffcb85467b6148db45928de
Valentin Korolev <kor.val@inbox.ru>
2017-04-18 19:27:43 +0300

    Initial templating implementation

--- src/ng/plugins/edit-form/edit.form.panel.js
+++ src/ng/plugins/edit-form/edit.form.panel.js
@@ -1,5 +1,6 @@
 import PluginComponent from '../plugin.component';
 import RowEditor from 'core/edit/edit.row';
+import cellBuilder from 'ng/components/cell/cell.build';
 
 const Plugin = PluginComponent('edit-form-panel', {inject: []});
 class EditFormPanel extends Plugin {
@@ -8,7 +9,22 @@ class EditFormPanel extends Plugin {
 	}
 
 	onInit() {
+		const templateScope = this.$scope.$new();
+		const editor = angular.element('<div layout="column"></div>');
+
 		this.editor = new RowEditor(this.$row, this.model.data().columns);
+		this.editor.editors.map(e => {
+			const build = cellBuilder(this.template, 'edit');
+			const link = build('body', this.model, e.column);
+			const element = angular.element('<div></div>');
+
+			link(element, templateScope);
+			editor.append(element);
+		});
+
+		const cur = this.$element[0];
+		const ch = cur.children[0].children[0];
+		angular.element(ch).append(editor);
 	}
 }
 
