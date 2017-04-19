2dae72f119064aaaf9e7cd1fb047cd102d10f9a9
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-04-19 05:01:54 +0300

    remove columns from the data triggers, gather statistics on column list update to track changes

--- src/themes/material/templates/body.cell.reference.edit.js
+++ src/themes/material/templates/body.cell.reference.edit.js
@@ -1,8 +1,27 @@
 ReferenceEdit.$inject = ['$scope', 'qgrid'];
 export default function ReferenceEdit($scope, qgrid) {
 	this.gridModel = qgrid.model();
+	const service = qgrid.service(this.gridModel);
+
 	this.commit = ($cell, $event) => {
 		$scope.$view.edit.cell.value = this.gridModel.selection().items;
 		$scope.$view.edit.cell.commit.execute($cell, $event);
+	};
+
+	const fetch = $scope.$view.edit.cell.fetch;
+	if (fetch.busy) {
+		const cancelBusy = service.busy();
+		fetch.busy
+			.then(() => {
+				this.gridModel.data({
+					rows: fetch.result
+				});
+				cancelBusy();
+			});
+	}
+	else {
+		this.gridModel.data({
+			rows: fetch.result
+		});
 	}
 }
