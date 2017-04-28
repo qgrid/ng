e3750ae5fb01701bcfb18e53652e29bdb91debc8
etokareva <etokareva1990@gmail.com>
2017-04-28 16:53:05 +0300

    download file

--- src/ng/plugins/export/export.js
+++ src/ng/plugins/export/export.js
@@ -11,15 +11,42 @@ TemplatePath
 		};
 	});
 
-const Plugin = PluginComponent('export');
+const Plugin = PluginComponent('export', {inject: ['$window', '$document']});
 class Export extends Plugin {
 	constructor() {
 		super(...arguments);
 		this.csv = new Command({
+			canExecute: () => this.type.toLowerCase() === 'csv',
 			execute: () => {
-				alert('export to ' + this.type);
+				const csv = this._toCsv();
+				this._download(csv);
 			}
 		});
+	}
+
+	get rows() {
+		return this.model.data().rows;
+	}
+
+	get columns() {
+		return this.model.data().columns;
+	}
+
+	_toCsv() {
+		return this.columns.map(column => column.title).join(",");
+	}
+
+	_download(csv) {
+		const csvFile = new Blob([csv], {type: "text/csv"});
+		const downloadLink = this.$document[0].createElement("a");
+		const body = this.$document[0].body;
+
+		downloadLink.download = 'exportTable.csv';
+		downloadLink.href = this.$window.URL.createObjectURL(csvFile);
+		downloadLink.style.display = "none";
+		body.appendChild(downloadLink);
+		downloadLink.click();
+		body.removeChild(downloadLink);
 	}
 }
 
