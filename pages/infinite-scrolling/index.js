ce5e5e8ad033426016e7aedcf95e66bd93b458b1
qgrid <qgrid.team@gmail.com>
2017-04-28 19:34:43 +0300

    (deploy): CI

---
+++ pages/infinite-scrolling/index.js
@@ -0,0 +1,30 @@
+Controller.$inject = ['$http', 'qgrid'];
+export default function Controller($http, qgrid) {
+	const ctrl = this;
+	const model = qgrid.model();
+	let rows = [];
+
+	ctrl.model = model;
+
+	model.pagination({
+		size: 20
+	});
+
+	model.scroll({
+		mode: 'virtual'
+	});
+
+	ctrl.model.data({
+		pipe: [(data, context, next) => {
+			const paginationState = model.pagination();
+			const size = paginationState.size;
+			const current = paginationState.current;
+			const start = current * size;
+
+			$http.get('data/people/100.json')
+				.then(response =>
+					next(rows = rows.concat(response.data.slice(start, start + size))));
+		}]
+			.concat(qgrid.pipeUnit.view)
+	});
+}
