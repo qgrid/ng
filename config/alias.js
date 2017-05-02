7b63b6aeb909cf43edadf76d7d8a2aef5ac6569e
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-05-02 17:10:26 +0300

    Added alias.js

--- config/alias.js
+++ config/alias.js
@@ -5,6 +5,6 @@ const path = require('path');
 System.config({
   src: path.resolve(__dirname, '../src'),
   core: path.resolve(__dirname, '../src/core'),
-  ng: path.resolve(__dirname, '../src/ng2'),
+  ng: path.resolve(__dirname, '../src/ng'),
   themes: path.resolve(__dirname, '../src/themes')
 });
