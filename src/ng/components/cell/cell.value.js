238f15ba0eefe5d7b9c2041ecc8a1ede0cf6b0c7
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-05-02 18:08:09 +0300

    stringify impl

--- src/ng/components/cell/cell.value.js
+++ src/ng/components/cell/cell.value.js
@@ -1,7 +1,31 @@
 import Directive from 'ng/directives/directive';
 import {CELL_VALUE_NAME} from 'ng/definition';
 
-const stringify = angular.$$stringify; // eslint-disable-line angular/no-private-call
+const isDate = angular.isDate;
+const toJson = angular.toJson;
+const hasCustomToString = angular.hasCustomToString;
+
+const stringify = value => {
+	if (value == null) { // null || undefined
+		return '';
+	}
+
+	switch (typeof value) {
+		case 'string':
+			break;
+		case 'number':
+			value = '' + value;
+			break;
+		default:
+			if (hasCustomToString(value) && !isArray(value) && !isDate(value)) {
+				value = value.toString();
+			} else {
+				value = toJson(value);
+			}
+	}
+
+	return value;
+};
 
 class CellValue extends Directive(CELL_VALUE_NAME) {
 	constructor($scope, $element) {
