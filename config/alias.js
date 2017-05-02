968bf7cd0a7057d7a2b3010e4940f27f5c41c8a7
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-05-02 17:07:07 +0300

    Added alias.js

---
+++ config/alias.js
@@ -0,0 +1,10 @@
+// This SystemJS fake config allows to web storm resolve aliases that are used from webpack
+
+const path = require('path');
+
+System.config({
+  src: path.resolve(__dirname, '../src'),
+  core: path.resolve(__dirname, '../src/core'),
+  ng: path.resolve(__dirname, '../src/ng2'),
+  themes: path.resolve(__dirname, '../src/themes')
+});
