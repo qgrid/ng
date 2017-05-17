8b60e06410b1466afbc3072e467f40208afa0f6c
etokareva <etokareva1990@gmail.com>
2017-05-17 05:21:32 +0300

    code refactoring

--- src/core/export/csv.js
+++ src/core/export/csv.js
@@ -2,35 +2,32 @@ import {getFactory as valueFactory} from '../services/value';
 
 const DELIMETER = ',';
 
-function Reverse(A) {
-	const AT = [];
-	const result = [];
-	for (let i = 0; i < A[0].length; i++) {
-		AT[i] = [];
-		for (let j = 0; j < A.length; j++) {
-			AT[i][j] = A[j][i];
-		}
-		result.push(AT[i].join(DELIMETER));
-	}
+function escape(value) {
+	let result = '' + value;
+	result = result.replace(/"/g, '""');
+	result = /[\n",]/.test(result) ? `"${result}"` : result;
 	return result;
 }
 
 export class Csv {
 	write(rows, columns) {
-		const data = [];
-		const head = [];
-		for (let j = 0; j < columns.length; j++) {
-			data.push([]);
-			head[j] = columns[j].title;
-			const value = valueFactory(columns[j]);
-			let result = '';
-			for (let i = 0; i < rows.length; i++) {
-				result = '' + value(rows[i]);
-				result = result.replace(/"/g, '""');
-				result = /[\n",]/.test(result) ? `"${result}"` : result;
-				data[j][i] = result;
-			}
+		const result = [];
+		const values = [];
+		let head = [];
+		for (let column of columns) {
+			values.push(valueFactory(column));
+			head.push(escape(column.title));
 		}
-		return [head.join(DELIMETER), ...Reverse(data)].join('\n');
+		result.push(head.join(DELIMETER));
+
+		for (let row of rows) {
+			const line = [];
+			for (let getValue of values) {
+				line.push(escape(getValue(row)));
+			}
+			result.push(line.join(DELIMETER));
+		}
+
+		return result.join('\n');
 	}
 }
