8350b34b3e1088e03810be9d34234bcefccf73bd
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-05-23 19:14:02 +0300

    fix header layout on pin

--- src/view/filters/pin.js
+++ src/view/filters/pin.js
@@ -1,10 +1,5 @@
 import {PadColumn} from '@grid/core/column-type';
 
 export default function pin(columns, pin) {
-	const result = columns.filter(c => c.model.pin === pin);
-	// if (pin) {
-	// 	result.push(new PadColumn(PadColumn.model({key: `$pad-${pin}`})));
-	// }
-
-	return result;
+	return columns.filter(c => c.model.pin === pin);
 }
