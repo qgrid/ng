fd09713e9dbf6854918645d0052e3e2e1ed3d954
Ekaterina Tokareva <etokareva1990@gmail.com>
2017-05-30 13:42:20 +0300

    after xml and xsd validty check improvements

--- src/core/export/xml.js
+++ src/core/export/xml.js
@@ -1,17 +1,25 @@
 import {isObject, isArray, isString} from '@grid/core/services/utility';
 
 const begin = '<?xml version="1.0" encoding="UTF-8"?><?mso-application progid="Excel.Sheet"?>';
-const schema = '<!-- START OF SCHEMA --><xsd:schema xmlns:xsd="http://www.w3.org/2001/XMLSchema"><xsd:element name="rows">' +
-	'<xsd:complexType><xsd:sequence><xsd:element name="row" maxOccurs="unbounded"><xsd:complexType><xsd:sequence>' +
-	'<xsd:element name="name"><xsd:complexType><xsd:sequence><xsd:element name="first" type="xsd:string"/>' +
-	'<xsd:element name="last" type="xsd:string"/></xsd:sequence></xsd:complexType></xsd:element><xsd:element name="gender" type="xsd:string"/>' +
-	'<xsd:element name="birthday" type="xsd:date"/><xsd:element name="contact"><xsd:complexType><xsd:sequence><xsd:element name="address">' +
-	'<xsd:complexType><xsd:sequence><xsd:element name="street" type="xsd:string"/><xsd:element name="zip" type="xsd:string"/>' +
-	'<xsd:element name="city" type="xsd:string"/><xsd:element name="state" type="xsd:string"/></xsd:sequence></xsd:complexType>' +
-	'</xsd:element><xsd:element name="email" type="xsd:string" maxOccurs="unbounded"/><xsd:element name="region" type="xsd:string" maxOccurs="unbounded"/>' +
-	'<xsd:element name="phone" type="xsd:string" maxOccurs="unbounded"/></xsd:sequence></xsd:complexType></xsd:element>' +
-	'<xsd:element name="likes" type="xsd:string" maxOccurs="unbounded"/><xsd:element name="memberSince" type="xsd:date"/></xsd:sequence>' +
-	'</xsd:complexType></xsd:element></xsd:sequence></xsd:complexType></xsd:element></xsd:schema><!-- END OF SCHEMA -->';
+const schema = '<xsd:schema xmlns:xsd="http://www.w3.org/2001/XMLSchema"><xsd:element name="rows"><xsd:complexType>'+
+	'<xsd:sequence><xsd:element name="head"><xsd:complexType><xsd:sequence><xsd:element name="column" maxOccurs="unbounded">'+
+	'<xsd:complexType><xsd:sequence><xsd:element name="key" type="xsd:string" minOccurs="1" maxOccurs="1"/>'+
+	'<xsd:element name="title" type="xsd:string" minOccurs="1" maxOccurs="1"/><xsd:element name="type" type="xsd:string" minOccurs="1" maxOccurs="1"/>'+
+	'</xsd:sequence></xsd:complexType></xsd:element></xsd:sequence></xsd:complexType></xsd:element><xsd:element name="body">'+
+	'<xsd:complexType><xsd:sequence><xsd:element name="row" minOccurs="0" maxOccurs="unbounded"><xsd:complexType>'+
+	'<xsd:sequence><xsd:element name="name" minOccurs="0" maxOccurs="1"><xsd:complexType><xsd:sequence>'+
+	'<xsd:element name="first" type="xsd:string" minOccurs="1" maxOccurs="1"/>'+
+	'<xsd:element name="last" type="xsd:string" minOccurs="1" maxOccurs="1"/></xsd:sequence></xsd:complexType></xsd:element>'+
+	'<xsd:element name="gender" type="xsd:string" minOccurs="0" maxOccurs="1"/><xsd:element name="birthday" type="xsd:date" minOccurs="0" maxOccurs="1"/>'+
+	'<xsd:element name="contact" minOccurs="0" maxOccurs="1"><xsd:complexType><xsd:sequence><xsd:element name="address" minOccurs="0" maxOccurs="1">'+
+	'<xsd:complexType><xsd:sequence><xsd:element name="street" type="xsd:string" minOccurs="0" maxOccurs="1"/>'+
+	'<xsd:element name="zip" type="xsd:string" minOccurs="0" maxOccurs="1"/><xsd:element name="city" type="xsd:string" minOccurs="0" maxOccurs="1"/>'+
+	'<xsd:element name="state" type="xsd:string" minOccurs="0" maxOccurs="1"/></xsd:sequence></xsd:complexType></xsd:element>'+
+	'<xsd:element name="email" type="xsd:string" minOccurs="0" maxOccurs="unbounded"/><xsd:element name="region" type="xsd:string" minOccurs="0" maxOccurs="unbounded"/>'+
+	'<xsd:element name="phone" type="xsd:string" minOccurs="0" maxOccurs="unbounded"/></xsd:sequence></xsd:complexType>'+
+	'</xsd:element><xsd:element name="likes" type="xsd:string" minOccurs="0" maxOccurs="unbounded"/>'+
+	'<xsd:element name="memberSince" minOccurs="0" maxOccurs="1" type="xsd:date"/></xsd:sequence></xsd:complexType>'+
+	'</xsd:element></xsd:sequence></xsd:complexType></xsd:element></xsd:sequence></xsd:complexType></xsd:element></xsd:schema>';
 
 function escape(value) {
 	let result = '' + value;
@@ -45,19 +53,24 @@ function objToXml(obj) {
 	}
 	return result;
 }
+function formHead(columns) {
+	const headers = ['<head>'];
+	for (let column of columns) {
+		headers.push(`<column><key>${column.key}</key><title>${column.title}</title><type>${column.type}</type></column>`);
+	}
+	headers.push('</head>');
+	return headers.join('');
+}
 
 export class Xml {
 	write(rows, columns) {
 		const result = [`${begin}<rows xmlns:xsd="http://www.w3.org/2001/XMLSchema">${schema}`];
-		// const headers = [];
+		result.push(...formHead(columns), '<body>');
 		for (let row of rows) {
 			const item = objToXml(row);
 			result.push(`<row>${item}</row>`);
 		}
-		// for (let column of columns) {
-		// 	headers.push(column.title);
-		// }
-		result.push('</rows>');
+		result.push('</body></rows>');
 		return result.join('');
 	}
 }
