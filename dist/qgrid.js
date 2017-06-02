b34cc97947db15ef7657bfdddcafb046142d3947
Valentin Korolev <kor.val@inbox.ru>
2017-06-02 13:02:39 +0300

    Release v1.0.27

--- dist/qgrid.js
+++ dist/qgrid.js
@@ -11425,8 +11425,6 @@ var MultipleSelectionState = function (_SelectionState) {
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return SingleSelectionState; });
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -11470,11 +11468,6 @@ var SingleSelectionState = function (_SelectionState) {
 		key: 'clearCore',
 		value: function clearCore() {
 			this.item = null;
-		}
-	}, {
-		key: 'lookup',
-		value: function lookup(item, unit) {
-			return _get(SingleSelectionState.prototype.__proto__ || Object.getPrototypeOf(SingleSelectionState.prototype), 'lookup', this).call(this, [item], unit);
 		}
 	}]);
 
