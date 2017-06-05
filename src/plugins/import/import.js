69b9821fb41cf8a1d0354301791d0cc6db0fa03e
Ekaterina Tokareva <etokareva1990@gmail.com>
2017-06-05 20:08:04 +0300

    import xlsx view

--- src/plugins/import/import.js
+++ src/plugins/import/import.js
@@ -1,6 +1,8 @@
 import PluginComponent from '../plugin.component';
 import {IMPORT_NAME} from '../definition';
 import {TemplatePath} from '@grid/core/template';
+import {Command} from '@grid/core/infrastructure';
+
 TemplatePath
 	.register(IMPORT_NAME, () => {
 		return {
@@ -13,6 +15,13 @@ const Plugin = PluginComponent('import');
 class Import extends Plugin {
 	constructor() {
 		super(...arguments);
+		this.xlsx = new Command({
+			canExecute: () => this.type === 'xlsx',
+			execute: () => {
+				// const xlsx = new Xlsx();
+				alert('Import from excel');
+			}
+		});
 	}
 
 	get resource() {
