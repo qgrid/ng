b86a9906fc9d1cfd3fb98c9ce5c1340ccaa84654
Ekaterina Tokareva <etokareva1990@gmail.com>
2017-06-06 20:07:07 +0300

    xlsx import class

--- src/plugins/import/import.js
+++ src/plugins/import/import.js
@@ -2,17 +2,7 @@ import PluginComponent from '../plugin.component';
 import {IMPORT_NAME} from '../definition';
 import {TemplatePath} from '@grid/core/template';
 import {Command, EventListener} from '@grid/core/infrastructure';
-import XLSX from 'xlsx';
-
-function to_json(workbook) {
-	let result = [];
-	for (let sheetName of workbook.SheetNames) {
-		const partial = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName], {header: 'A'});
-		result = partial.concat(result);
-	}
-
-	return result;
-}
+import {Xlsx} from './xlsx';
 
 TemplatePath
 	.register(IMPORT_NAME, () => {
@@ -31,22 +21,14 @@ class Import extends Plugin {
 	}
 
 	handleFile(e) {
+		const xlsx = new Xlsx();
 		const files = e.target.files;
 		for (let file of files) {
 			const reader = new FileReader();
 			reader.onload = e => {
-				const columns = [];
 				const data = e.target.result;
-				const workbook = XLSX.read(data, {type: 'binary'});
-				const json = to_json(workbook);
-				const headers = Object.keys(json[0]);
-				for (let header of headers) {
-					columns.push({
-						key: header,
-						title: header
-					});
-				}
-				this.model.data({rows: json, columns: columns});
+				const result = xlsx.read(data);
+				this.model.data(result);
 			};
 			reader.readAsBinaryString(file);
 		}
---
+++ src/plugins/import/xlsx.js
@@ -0,0 +1,30 @@
+import XLSX from 'xlsx';
+
+function to_json(workbook) {
+	let result = [];
+	for (let sheetName of workbook.SheetNames) {
+		const partial = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName], {header: 'A'});
+		result = partial.concat(result);
+	}
+
+	return result;
+}
+
+export class Xlsx {
+	read(data) {
+		const columns = [];
+		const workbook = XLSX.read(data, {type: 'binary'});
+		const json = to_json(workbook);
+		const headers = Object.keys(json[0]);
+		for (let header of headers) {
+			columns.push({
+				key: header,
+				title: header
+			});
+		}
+		return {
+			columns: columns,
+			rows: json
+		};
+	}
+}
