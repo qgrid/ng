243a26a32247f9e79fb91e26918be320838e41f3
Andrey <andrey.matuschenko@arcadia.spb.ru>
2017-06-13 16:51:49 +0300

    Fixed bucket.d.ts

--- src/core/services/aggregation.d.ts
+++ src/core/services/aggregation.d.ts
@@ -4,22 +4,22 @@ export declare class Aggregation {
 
 	constructor();
 
-	static first(rows: Row[], getValue: (object) => any ) any;
+	static first(rows: Row[], getValue: () => any ) any;
 
-	static last(rows: any[], getValue: () => any) any;
+	static last(rows: Row[], getValue: () => any) any;
 
-	static max(rows: any[], getValue: () => any) any;
+	static max(rows: Row[], getValue: () => any) any;
 
-	static min(rows: any[], getValue: () => any) any;
+	static min(rows: Row[], getValue: () => any) any;
 
-	static minMax(rows: any[], getValue: () => any) any;
+	static minMax(rows: Row[], getValue: () => any) any;
 
-	static avg(rows: any[], getValue: () => any, options: any[]) any;
+	static avg(rows: Row[], getValue: () => any, options: any[]) any;
 
-	static sum(rows: any[], getValue: () => any, options: any[], container: any) any;
+	static sum(rows: Row[], getValue: () => any, options: any[], container: any) any;
 
-	static join(rows: any[], getValue: () => any, options: any[]) any;
+	static join(rows: Row[], getValue: () => any, options: any[]) any;
 
-	static count(rows: any[], getValue: () => any, options: any[]) any;
+	static count(rows: Row[], getValue: () => any, options: any[]) any;
 
 }
