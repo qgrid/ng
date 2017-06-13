55429dd92be671032f31bad07f09cf05fbd783ac
Ekaterina Tokareva <etokareva1990@gmail.com>
2017-06-13 19:31:57 +0300

    xml import

--- src/core/import/xml.js
+++ src/core/import/xml.js
@@ -1,31 +1,52 @@
-const schema = require('raw-loader!@grid/core/export/schema.xml');
-function xmlToObj(xml) {
+// const schema = require('raw-loader!@grid/core/export/schema.xml');
+
+const KEY_DELIMETER = '-';
+
+function flatView(xml) {
 	const result = {};
-	result[xml.nodeName] = xml.nodeValue;
-	if (xml.hasChildNodes()) {
-		xmlToObj();
+	if (xml.nodeType === 1 && xml.hasChildNodes()) {
+		for (let child of xml.childNodes) {
+			if (child.nodeType === 1) {
+				const flatObject = flatView(child);
+				for (let [flatProp, flatValue] of Object.entries(flatObject)) {
+					if (flatProp === '#text') {
+						result[child.nodeName] = flatValue;
+					} else {
+						result[child.nodeName + KEY_DELIMETER + flatProp] = flatValue;
+					}
+				}
+			} else if (child.nodeType === 3) {
+				result[child.nodeName] = child.nodeValue;
+			}
+		}
+		return result;
+	} else if (xml.nodeType === 3) {
+		result[xml.nodeName] = xml.nodeValue;
 	}
-	return result;
 }
 
 function parseLight(root) {
 	const rows = root.getElementsByTagName('row');
+	const result = [];
 	for (let row of rows) {
-		xmlToObj(row);
+		result.push(flatView(row));
 	}
+	console.log(result);
+	return result;
 }
 
 export class Xml {
 	read(data) {
 		const parser = new DOMParser();
-		const xsd = parser.parseFromString(schema, 'text/xml').firstChild;
 		const root = parser.parseFromString(data, 'text/xml').documentElement;
-		const dataXsd = root.getElementsByTagNameNS('*', 'schema').item(0);
+		console.log(root);
+		// const xsd = parser.parseFromString(schema, 'text/xml').firstChild;
+		// const dataXsd = root.getElementsByTagNameNS('*', 'schema').item(0);
 
-		if (xsd.isEqualNode(dataXsd)) {
-			parseLight(root);
-		}
+		// if (xsd.isEqualNode(dataXsd)) {
+		// 	parseLight(root);
+		// }
 
-		return root;
+		return parseLight(root);
 	}
 }
