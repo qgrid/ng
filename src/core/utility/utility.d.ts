2d7a8f38118594edd722fd126af0e2e61b9bb711
Andrey <andrey.matuschenko@arcadia.spb.ru>
2017-06-13 17:22:10 +0300

    Added utility.d.ts

---
+++ src/core/utility/utility.d.ts
@@ -0,0 +1,11 @@
+export declare const noop: object;
+
+export declare const yes: boolean;
+
+export declare const no: boolean;
+
+export declare const identity: object;
+
+export declare const toCamelCase: (...names: string[]) => string;
+
+export declare const isEmail: (value: string) => boolean;
