c53a9c3014fa2691dd02d3e4f95f09e654312092
Andrey <andrey.matuschenko@arcadia.spb.ru>
2017-06-14 20:18:00 +0300

    Fixed merge d.ts

--- src/core/services/merge.d.ts
+++ src/core/services/merge.d.ts
@@ -1,7 +1,14 @@
 ﻿interface IMergeResult{
-	updated: any;
-	removed: any;
-	inserted: any;
+	updated: number;
+	removed: number;
+	inserted: number;
 }
 
-export declare function merge(settings: object): IMergeResult;
+interface ISettings {
+	equals: (l: object, r: object) => boolean;
+	update: (l: object, r: object /*left, i*/) => void;
+	remove: (l: object, left: object, i: object) => void;
+	insert: (r: object, left: object) => void;
+}
+
+export declare function merge(settings: ISettings): IMergeResult;
