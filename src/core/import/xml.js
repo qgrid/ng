c25c491aea030a692955103335cbbaf6c6ab7b68
Ekaterina Tokareva <etokareva1990@gmail.com>
2017-06-14 14:26:55 +0300

    xml import array support

--- src/core/import/xml.js
+++ src/core/import/xml.js
@@ -2,27 +2,34 @@
 
 const KEY_DELIMETER = '-';
 
-function flatView(xml) {
+function flatView(xml, separator = ', ') {
 	const result = {};
 	if (xml.nodeType === 1 && xml.hasChildNodes()) {
 		for (let child of xml.childNodes) {
-			if (child.nodeType === 1) {
-				const flatObject = flatView(child);
-				for (let [flatProp, flatValue] of Object.entries(flatObject)) {
-					if (flatProp === '#text') {
-						result[child.nodeName] = flatValue;
-					} else {
-						result[child.nodeName + KEY_DELIMETER + flatProp] = flatValue;
+			const children = xml.getElementsByTagName(child.tagName);
+			if (children.length > 1) {
+				const items = [];
+				for (let i = 0; i < children.length; i++) {
+					items.push(children.item(i).textContent);
+					if (i) {
+						children.item(i).remove();
 					}
 				}
+				result[child.nodeName] = items.join(separator);
+			} else if (child.nodeType === 1) {
+				const flatObject = flatView(child, separator);
+				for (let [flatProp, flatValue] of Object.entries(flatObject)) {
+					const name = flatProp === '#text' ? child.nodeName : child.nodeName + KEY_DELIMETER + flatProp;
+					result[name] = flatValue;
+				}
 			} else if (child.nodeType === 3) {
-				result[child.nodeName] = child.nodeValue;
+				result[child.nodeName] = child.textContent;
 			}
 		}
-		return result;
 	} else if (xml.nodeType === 3) {
-		result[xml.nodeName] = xml.nodeValue;
+		result[xml.nodeName] = xml.textContent;
 	}
+	return result;
 }
 
 function parseLight(root) {
@@ -31,7 +38,6 @@ function parseLight(root) {
 	for (let row of rows) {
 		result.push(flatView(row));
 	}
-	console.log(result);
 	return result;
 }
 
@@ -39,7 +45,6 @@ export class Xml {
 	read(data) {
 		const parser = new DOMParser();
 		const root = parser.parseFromString(data, 'text/xml').documentElement;
-		console.log(root);
 		// const xsd = parser.parseFromString(schema, 'text/xml').firstChild;
 		// const dataXsd = root.getElementsByTagNameNS('*', 'schema').item(0);
 
