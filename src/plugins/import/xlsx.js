3e6733d0a624bf940b4ca255ac760a2c67bdadf3
Ekaterina Tokareva <etokareva1990@gmail.com>
2017-06-15 15:14:21 +0300

    fix rowNumber column and export-import-column-number-type bug

--- src/plugins/import/xlsx.js
+++ src/plugins/import/xlsx.js
@@ -1,18 +1,25 @@
 import XLSX from 'xlsx';
 
-function to_json(workbook) {
+function toJson(workbook) {
 	let result = [];
 	for (let sheetName of workbook.SheetNames) {
 		const partial = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName], {header: 'A'});
 		result = partial.concat(result);
 	}
 
+	return result.map(rewriteObject);
+}
+function rewriteObject(obj) {
+	const result = {};
+	for (let [key, value] of Object.entries(obj)) {
+		result[key] = value;
+	}
 	return result;
 }
 
 export class Xlsx {
 	read(data) {
 		const workbook = XLSX.read(data, {type: 'binary'});
-		return to_json(workbook);
+		return toJson(workbook);
 	}
 }
