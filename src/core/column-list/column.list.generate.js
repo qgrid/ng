f7a342581eb35530f06a6b52019b5f37144c1fa4
Ekaterina Tokareva <etokareva1990@gmail.com>
2017-06-19 20:19:36 +0300

    generate context

--- src/core/column-list/column.list.generate.js
+++ src/core/column-list/column.list.generate.js
@@ -1,15 +1,16 @@
 import {startCase} from '../utility';
 import {compile, getType} from '../services';
 
-export function generate(rows, columnFactory, deep = true) {
-	if (!rows || rows.length === 0) {
+export function generate(context) {
+	const deep = context.deep || true;
+	if (!context.rows || context.rows.length === 0 || !context.columnFactory) {
 		return [];
 	}
 
-	return build(rows[0], null, columnFactory, deep);
+	return build(context.rows[0], null, context.columnFactory, deep, context.title);
 }
 
-function build(graph, path, columnFactory, deep) {
+function build(graph, path, columnFactory, deep, title) {
 	const props = Object.getOwnPropertyNames(graph);
 	return props.reduce((columns, prop) => {
 		const value = graph[prop];
@@ -19,7 +20,7 @@ function build(graph, path, columnFactory, deep) {
 			case 'array': {
 				const column = columnFactory(type).model;
 				column.key = propPath;
-				column.title = startCase(propPath);
+				column.title = title ? title(columns.length, graph) : startCase(propPath);
 				column.path = propPath;
 				column.value = compile(propPath);
 				column.source = 'generation';
@@ -31,14 +32,14 @@ function build(graph, path, columnFactory, deep) {
 			}
 			case 'object': {
 				if (deep) {
-					columns.push(...build(value, propPath, columnFactory, true));
+					columns.push(...build(value, propPath, columnFactory, true, title));
 				}
 				break;
 			}
 			default: {
 				const column = columnFactory(type).model;
 				column.key = propPath;
-				column.title = startCase(propPath);
+				column.title = title ? title(columns.length, graph) : startCase(propPath);
 				column.path = propPath;
 				column.value = compile(propPath);
 				column.source = 'generation';
