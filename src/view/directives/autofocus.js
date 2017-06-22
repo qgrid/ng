124fd59f09bc5ec954436ae2d538762676ccab16
Maxim Titov <maxim.titov@arcadia.spb.ru>
2017-06-22 14:30:37 +0300

    use grid table inside autofocus

--- src/view/directives/autofocus.js
+++ src/view/directives/autofocus.js
@@ -1,5 +1,6 @@
 import Directive from './directive';
 import {AUTOFOCUS_NAME, GRID_NAME} from '@grid/view/definition';
+import {noop, isUndefined} from '@grid/core/utility';
 
 class Autofocus extends Directive(AUTOFOCUS_NAME, {root: `${GRID_NAME}`}) {
 	constructor($scope, $element, $timeout) {
@@ -8,25 +9,46 @@ class Autofocus extends Directive(AUTOFOCUS_NAME, {root: `${GRID_NAME}`}) {
 		this.$scope = $scope;
 		this.$element = $element;
 		this.$timeout = $timeout;
+
+		this.dataChangedOff = noop;
 	}
 
 	onInit() {
-		this.model.dataChanged.on(e => {
-			if (e.hasChanges('rows') && e.state.rows.length > 0) {
+		this.dataChangedOff = this.model.viewChanged.watch(e => {
+			if (e.hasChanges('rows') && e.state.rows.length > 0
+				|| e.hasChanges('nodes') && e.state.nodes.length > 0) {
 				this.$timeout(() => {
-					const table = this.$element.find('table')[0];
-					table.focus();
+					if (this.table.body.rowCount === 0) {
+						return;
+					}
+
+					const table = Object.keys(this.table.markup).find(p => p.startsWith('table'));
+					if (isUndefined(table)) {
+						return;
+					}
+
+					this.table.markup[table].focus();
 					this.model.focus({
 						rowIndex: 0,
 						columnIndex: 0
 					});
 				}, 200);
+
+				this.dataChangedOff();
 			}
 		});
 	}
 
 	get model() {
 		return this.root.model;
+	}
+
+	get table() {
+		return this.root.table;
+	}
+
+	onDestroy() {
+		this.dataChangedOff();
 	}
 }
 
