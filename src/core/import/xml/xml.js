c7c64d6ba0b7c03947029a4fa394b0c886d85a7b
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-06-29 22:21:04 +0300

    Fix xml

--- src/core/import/xml/xml.js
+++ src/core/import/xml/xml.js
@@ -14,121 +14,100 @@ export class Xml {
 		}
 		const parser = new DOMParser();
 		const root = parser.parseFromString(text, 'text/xml').documentElement;
-
-		this.elementsFromAttributes(root);
 		const statistics = this.getStatistics(root);
 		const graph = this.build(root, statistics, 'root');
-
-		if (isArray(graph)) {
-			return graph;
+		const key = Object.keys(graph)[0];
+		const result = graph[key];
+		if (isArray(result)) {
+			return result
 		}
 
-		return [graph];
+		return [result];
+	}
+
+	arrayFromChildren(node, statistics, path, tag) {
+		const result = [];
+		const children = Array.from(node.children).filter(child => child.nodeName === tag);
+		for (let child of children) {
+			result.push(this.buildNonArray(child, statistics, path));
+		}
+
+		return result;
+	}
+
+	build(node, statistics, path = 'root') {
+		const st = statistics.get(path);
+		if (st.isArray) {
+			return this.arrayFromChildren(node.parentNode, statistics, path, node.nodeName);
+		}
+
+		return this.buildNonArray(node, statistics, path);
+	}
+
+	buildNonArray(node, statistics, path = 'root') {
+		const st = statistics.get(path);
+		if (st.isObject) {
+			const result = {};
+			const visited = new Set();
+			for (let attr of Array.from(node.attributes)) {
+				result[attr.name] = attr.value;
+			}
+
+			for (let child of Array.from(node.children)) {
+				const childPath = this.getPath(path, child.nodeName);
+				if (visited.has(childPath)) {
+					continue;
+				}
+
+				visited.add(childPath);
+				result[child.nodeName] = this.build(child, statistics, childPath)
+			}
+
+			return result;
+		}
+
+		if (st.isText) {
+			return node.textContent;
+		}
+
+		return null;
+	}
+
+	info(node, lastInfo) {
+		if (!lastInfo) {
+			lastInfo = {
+				isArray: false,
+				isObject: false,
+				isText: false
+			};
+		}
+
+		return {
+			isArray: lastInfo.isArray || Array.from(node.parentNode.children).filter(child => child.nodeName === node.nodeName).length > 1,
+			isObject: lastInfo.isObject || node.children.length > 0 || node.attributes.length > 0,
+			isText: lastInfo.isText || this.isTextContainer(node)
+		};
+	}
+
+	getStatistics(node, path = 'root', statistics = new Map()) {
+		statistics.set(path, this.info(node, statistics.get(path)));
+
+		const children = Array.from(node.children);
+		if (children.length > 0) {
+			for (let child of children) {
+				const childPath = this.getPath(path, child.nodeName);
+				this.getStatistics(child, childPath, statistics);
+			}
+		}
+
+		return statistics;
 	}
 
 	isTextContainer(node) {
 		return node.nodeType === NODE_TYPE.ELEMENT && !node.children.length && node.childNodes.length;
 	}
 
-	hasAttributes(node) {
-		return Array.from(node.attributes).length > 0;
-	}
-
-	elementsFromAttributes(node) {
-		if (this.hasAttributes(node)) {
-			const owner = node.ownerDocument;
-			for (let attr of Array.from(node.attributes)) {
-				const element = owner.createElement(attr.name);
-				const text = owner.createTextNode(attr.value);
-				element.appendChild(text);
-				node.appendChild(element);
-				node.removeAttributeNode(attr);
-			}
-		}
-		const children = Array.from(node.children);
-		if (children.length > 0) {
-			for (let child of children) {
-				this.elementsFromAttributes(child);
-			}
-		}
-		return node;
-	}
-
-	objectFromChildren(node, statistics, path) {
-		const graph = {};
-		for (let child of Array.from(node.children)) {
-			const childPath = `${path}/${child.nodeName}`;
-			const testSt = statistics.get(childPath);
-			const result = testSt.isArray
-				? this.arrayFromParent(child.parentNode, statistics, path, child.nodeName)
-				: this.build(child, statistics, childPath);
-
-			graph[child.nodeName] = result;
-		}
-
-		return graph;
-	}
-
-	arrayFromChildren(node, statistics, path) {
-		const result = [];
-		for (let child of Array.from(node.children)) {
-			const childPath = `${path}/${child.nodeName}`;
-			result.push(this.build(child, statistics, childPath));
-		}
-
-		return result;
-	}
-
-	arrayFromParent(node, statistics, path, tag) {
-		const result = [];
-		const childPath = `${path}/${tag}`;
-		for (let child of Array.from(node.getElementsByTagName(tag))) {
-			result.push(this.build(child, statistics, childPath));
-		}
-
-		return result;
-	}
-
-
-	build(node, statistics, path = 'root') {
-		if (node.children.length) {
-			const testPath = `${path}/${node.children.item(0).nodeName}`;
-			const testSt = statistics.get(testPath);
-			const result = testSt.isArray
-				? this.arrayFromChildren(node, statistics, path)
-				: this.objectFromChildren(node, statistics, path);
-
-			return result;
-		}
-		else if (this.isTextContainer(node)) {
-			return node.textContent;
-		}
-
-		return node.nodeValue;
-	}
-
-	info(node, lastInfo) {
-		if (lastInfo && lastInfo.isArray) {
-			return lastInfo;
-		}
-
-		return {
-			isArray: node.parentNode.getElementsByTagName(node.nodeName).length > 1
-		};
-	}
-
-	getStatistics(node, path = 'root', statistics = new Map()) {
-
-		statistics.set(path, this.info(node, statistics.get(path)));
-
-		const children = Array.from(node.children);
-		if (children.length > 0) {
-			for (let child of children) {
-				const childPath = `${path}/${child.nodeName}`;
-				this.getStatistics(child, childPath, statistics);
-			}
-		}
-
-		return statistics;
+	getPath(...args) {
+		return args.join('/');
 	}
 }
