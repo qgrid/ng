d48358b61af21f00893b7c40a30e10bea99e4e72
Ekaterina Tokareva <etokareva1990@gmail.com>
2017-06-30 17:02:57 +0300

    action plugin

--- demo/pages/action-model/index.js
+++ demo/pages/action-model/index.js
@@ -6,35 +6,37 @@ export default function Controller($http, qgrid) {
 
 	this.rows = [];
 
-	ctrl.addCommand = new qgrid.Command({
-		execute: () => {
-			const newRow = {
-				'id': guid(),
-				'name': {
-					'first': null,
-					'last': null
-				},
-				'gender': 'male',
-				'birthday': null,
-				'contact': {
-					'address': {
-						'street': null,
-						'zip': null,
-						'city': null,
-						'state': null
+	ctrl.addCommand = () => {
+		return new qgrid.Command({
+			execute: () => {
+				const newRow = {
+					'id': guid(),
+					'name': {
+						'first': null,
+						'last': null
 					},
-					'email': [],
-					'region': null,
-					'phone': []
-				},
-				'likes': [],
-				'memberSince': null
-			};
+					'gender': 'male',
+					'birthday': null,
+					'contact': {
+						'address': {
+							'street': null,
+							'zip': null,
+							'city': null,
+							'state': null
+						},
+						'email': [],
+						'region': null,
+						'phone': []
+					},
+					'likes': [],
+					'memberSince': null
+				};
 
-			this.rows = [newRow].concat(this.rows);
-		},
-		shortcut: 'F7'
-	});
+				this.rows = [newRow].concat(this.rows);
+			},
+			shortcut: 'F7'
+		});
+	};
 
 
 	$http.get('data/people/10.json')
--- src/plugins/action/action.js
+++ src/plugins/action/action.js
@@ -45,6 +45,6 @@ export default Action.component({
 		'type': '@',
 		'icon': '@',
 		'title': '@',
-		'command': '='
+		'command': '&'
 	}
 });
