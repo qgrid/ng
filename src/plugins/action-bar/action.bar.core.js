9a4550921fe9c6af6cdd7e586899e0df483f0fbb
Ekaterina Tokareva <etokareva1990@gmail.com>
2017-07-07 14:33:51 +0300

    fix shortcut register for act bar

--- src/plugins/action-bar/action.bar.core.js
+++ src/plugins/action-bar/action.bar.core.js
@@ -21,7 +21,7 @@ class ActionBarCore extends Plugin {
 					}
 
 					this.shortcutOff = shortcut
-						.register('actionBarCore', e.state.items.map(act => act.command));
+						.register(e.state.items.map(act => act.command));
 				}
 			});
 		}
