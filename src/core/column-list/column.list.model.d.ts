07308659996918ea4a50a5b99839d8e7dc3feec3
munkk <munkk@yandex.ru>
2017-07-13 21:58:13 +0300

    Update column.list.model.d.ts

--- src/core/column-list/column.list.model.d.ts
+++ src/core/column-list/column.list.model.d.ts
@@ -1,5 +1,4 @@
 import {ColumnModel} from '../column-type/column.model';
-import {ReferenceColumn} from '../column-type/reference.column';
 
 export declare type GenerationMode = 'deep' | 'shallow';
 
@@ -8,5 +7,5 @@ export declare class ColumnListModel {
 	generation: GenerationMode;
 	index: string[];
 	columns: ColumnModel[];
-	reference: ReferenceColumn;
+	reference: object;
 }
