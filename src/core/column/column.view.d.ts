221a8f14e48c1a4314695d566c87c82b1e049366
munkk <munkk@yandex.ru>
2017-07-13 22:01:53 +0300

    Update column.view.d.ts

--- src/core/column/column.view.d.ts
+++ src/core/column/column.view.d.ts
@@ -1,8 +1,9 @@
 import {View} from '../view/view';
 import {Model} from '../infrastructure/model';
 import {GridService} from '../services/grid';
+import {GenerationMode} from '../column-list/column.list.model';
 
 export declare class ColumnView extends View {
 	constructor(model: Model, service: GridService);
-	updateOn(generation: string): boolean;
+	updateOn(generation: GenerationMode): boolean;
 }
