2564d8bafdd549ff5aba0bf1cea0952f3da0433a
munkk <munkk@yandex.ru>
2017-07-17 19:39:04 +0300

    Update data.model.d.ts

--- src/core/data/data.model.d.ts
+++ src/core/data/data.model.d.ts
@@ -1,9 +1,19 @@
 import {ColumnModel} from '../column-type/column.model';
+import {IDataPipe} from '../pipe/data.pipe';
+import {IFilterPipe} from '../pipe/filter.pipe';
+import {IPaginationPipe} from '../pipe/pagination.pipe';
+import {ISortPipe} from '../pipe/sort.pipe';
+import {IMemoPipe} from '../pipe/memo.pipe';
+import {IGroupPipe} from '../pipe/group.pipe';
+import {IPivotPipe} from '../pipe/pivot.pipe';
+import {IColumnPipe} from '../pipe/column.pipe';
+import {IViewPipe} from '../pipe/view.pipe';
+
 
 export declare class DataModel {
 	constructor();
 	rows: any[];
 	columns: ColumnModel[];
-	pipe: object[];
+	pipe: [IDataPipe, IFilterPipe, IPaginationPipe, ISortPipe, IMemoPipe, IGroupPipe, IPivotPipe, IColumnPipe, IViewPipe];
 	triggers: object[];
 }
