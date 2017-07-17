d36e75cb15da923de071f2748e97e6def9233077
munkk <munkk@yandex.ru>
2017-07-17 17:49:12 +0300

    Update command.manager.d.ts

--- src/core/command/command.manager.d.ts
+++ src/core/command/command.manager.d.ts
@@ -1,9 +1,7 @@
-import {IFunc} from '../dom/view';
 import {Command} from './command';
 
 export declare class CommandManager {
-	constructor(apply: IFunc);
-	apply: IFunc;
+	constructor();
 	invoke(commands: Command[]): boolean;
 	filter(commands: Command[]): Command[];
 }
