70c9149de775ed011c2ac0e8276605bf5f5a8cdd
munkk <munkk@yandex.ru>
2017-07-17 18:48:53 +0300

    Update data.model.d.ts

--- src/core/data/data.model.d.ts
+++ src/core/data/data.model.d.ts
@@ -1,18 +1,9 @@
 import {ColumnModel} from '../column-type/column.model';
 
-export interface ITriggers{
-	[data: string]: string[];
-	[pagination: string]: string[];
-	[sort: string]: string[];
-	[filter: string]: string[];
-	[group: string]: string[];
-	[pivot: string]: string[];
-}
-
 export declare class DataModel {
 	constructor();
 	rows: any[];
 	columns: ColumnModel[];
 	pipe: object[];
-	triggers: ITriggers;
+	triggers: object[];
 }
