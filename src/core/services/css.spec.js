608a7c35cd8b48c969e4b6714e9d660ffa0ddd24
Andrey <munkk@yandex.ru>
2017-08-01 18:19:57 +0300

    Css test

--- src/core/services/css.spec.js
+++ src/core/services/css.spec.js
@@ -9,14 +9,14 @@ describe('Css', () => {
 		it('should check whether Style tag was append to Head tag', () => {
 			testObject.set(testStyleObject);
 
-			let isStyleTagExist = document.querySelector('head > style#q-style') === null ? false : true;
+			let isStyleTagExist = document.querySelector('head > style#q-style') !== null;
 
 			expect(isStyleTagExist).to.be.equal(true);
 		});
 		it('should check whether Style tag was removed from Head tag', () => {
 			testObject.remove();
 
-			let isStyleTagExist = document.querySelector('head > style#q-style') === null ? false : true;
+			let isStyleTagExist = document.querySelector('head > style#q-style') !== null;
 
 			expect(isStyleTagExist).to.be.equal(false);
 		});
