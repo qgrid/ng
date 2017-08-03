72a3a55b432d1ac588e52fc4ae19f7251becef35
munkk <munkk@yandex.ru>
2017-08-03 16:10:56 +0300

    Update markup.spec.js

--- src/core/services/markup.spec.js
+++ src/core/services/markup.spec.js
@@ -3,7 +3,7 @@ import * as Markup from './markup';
 describe('markup.service', () => {
 	let testObject = {someStyle: {color: 'yellow'}, someStyle2: {visibility: 'hidden'}};
 
-	it('should make array with strings of styles from testObject', () => {
+	it('should make array which contains strings of styles from testObject', () => {
 		let arr = Markup.buildLines(testObject);
 
 		expect(arr[0]).to.equal('someStyle{\n\tcolor:yellow !important;\n}');
