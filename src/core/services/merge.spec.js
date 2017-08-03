c7d35210df2c17e5d7551145eb6918127c425146
Andrey <munkk@yandex.ru>
2017-08-03 21:27:28 +0300

    merge test

---
+++ src/core/services/merge.spec.js
@@ -0,0 +1,29 @@
+﻿import * as Merge from './merge';
+
+describe('merge.service', () => {
+
+	const testSettingsObject = {};
+
+	const foo = Merge.merge(testSettingsObject);
+
+	it('should return true if resultObject.updated === 3', () => {
+		const resultObject = foo([1,2,3], [1,2,3,40,50,60,70,80], []);
+		const res = resultObject.updated === 3;
+
+		expect(res).to.be.equal(true);
+	});
+
+	it('should return true if resultObject.inserted === 5', () => {
+		const resultObject = foo([1], [1,2,3,4,5,6], []);
+		const res = resultObject.inserted === 5;
+
+		expect(res).to.be.equal(true);
+	});
+
+	it('should return true if resultObject.removed === 5', () => {
+		const resultObject = foo([1,2,3,4,5,6], [1], []);
+		const res = resultObject.removed === 5;
+
+		expect(res).to.be.equal(true);
+	});
+});
