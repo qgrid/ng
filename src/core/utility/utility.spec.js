37f4f88b7a849faddc0e410d20e9b69a580b1e4b
Andrey <munkk@yandex.ru>
2017-08-15 13:59:33 +0300

    Utility test

--- src/core/utility/utility.spec.js
+++ src/core/utility/utility.spec.js
@@ -1,4 +1,5 @@
 import {isObject, startCase} from './utility';
+import * as Utility from './utility';
 
 describe('utility', () => {
 	describe('isObject', () => {
@@ -25,6 +26,52 @@ describe('utility', () => {
 		it('should return capitalized abbreviation', () => {
 			const withAbbr = 'userTIN';
 			expect(startCase(withAbbr)).to.equal('User TIN');
+		});
+	});
+
+	describe('noop', () => {
+		let foo = Utility.noop();
+		it('should check whether the noop function returns nothing', () => {
+			expect(foo).to.equal(undefined);
+		});
+	});
+
+	describe('yes', () => {
+		let foo = Utility.yes();
+		it('should check whether the yes function returns true', () => {
+			expect(foo).to.equal(true);
+		});
+	});
+
+	describe('no', () => {
+		let foo = Utility.no();
+		it('should check whether the no function returns false', () => {
+			expect(foo).to.equal(false);
+		});
+	});
+
+	describe('identity', () => {
+		let foo = Utility.identity(123);
+		it('should check whether the identity function returns its argument', () => {
+			expect(foo).to.equal(123);
+		});
+	});
+
+	describe('toCamelCase', () => {
+		let foo = Utility.toCamelCase('camel', 'case');
+		it('should return string transformed into camelCase', () => {
+			expect(foo).to.equal('camelCase');
+		});
+	});
+
+	describe('isEmail', () => {
+		it('should validate an email address', () => {
+			let foo = Utility.isEmail('1email1@gmail.com');
+			expect(foo).to.equal(true);
+		});
+		it('should return false if passed incorrect email', () => {
+			let foo = Utility.isEmail('@email.com');
+			expect(foo).to.equal(false);
 		});
 	});
 });
