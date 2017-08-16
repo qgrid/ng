ae3b39f9eb1166107b7c66b272b18aeba220299b
munkk <munkk@yandex.ru>
2017-08-16 14:23:44 +0300

    Update table.command.manager.spec.js

--- src/core/command/table.command.manager.spec.js
+++ src/core/command/table.command.manager.spec.js
@@ -1,7 +1,7 @@
 import {TableCommandManager} from './table.command.manager';
 
 describe('CompositeCommandManager', () => {
-	let arrayOfCommands = [],
+	let commands = [],
 		i = 5,
 		arr = [],
 		tableWithFocus = {view: {isFocused: () => true}},
@@ -10,12 +10,12 @@ describe('CompositeCommandManager', () => {
 
 	while (i > 0) {
 		if (i % 2 === 0) {
-			arrayOfCommands.push({
+			commands.push({
 				execute: () => arr.push(1),
 				canExecute: () => true
 			});
 		} else {
-			arrayOfCommands.push({
+			commands.push({
 				execute: () => arr.push(2),
 				canExecute: () => false
 			});
@@ -26,13 +26,13 @@ describe('CompositeCommandManager', () => {
 	describe('filter', () => {
 		it('should return filtered commands if ViewActive = true', () => {
 			let tableCommandManager = new TableCommandManager(apply, tableWithFocus);
-			let filtered = tableCommandManager.filter(arrayOfCommands);
+			let filtered = tableCommandManager.filter(commands);
 			expect(filtered.length).to.equal(2);
 		});
 
 		it('should return empty array if ViewActive = false', () => {
 			let tableCommandManager = new TableCommandManager(apply, tableWithoutFocus);
-			let filtered = tableCommandManager.filter(arrayOfCommands);
+			let filtered = tableCommandManager.filter(commands);
 			expect(filtered.length).to.equal(0);
 		});
 	});
