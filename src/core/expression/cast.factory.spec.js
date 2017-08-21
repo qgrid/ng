f89624e1f376dfb21f5faeb8a510b31262ac18f5
munkk <munkk@yandex.ru>
2017-08-21 19:47:41 +0300

    Update cast.factory.spec.js

--- src/core/expression/cast.factory.spec.js
+++ src/core/expression/cast.factory.spec.js
@@ -23,7 +23,19 @@ describe('CastFactory', () => {
 		it('should convert string to Date', () => {
 			let foo = CastFactory.castFactory('2015-05-05');
 			let result = foo(new Date(2017));
-			expect(result instanceof Date).to.equal(true);
+			expect(result).to.be.an.instanceof(Date);
+		});
+
+		it('should convert string to number', () => {
+			let foo = CastFactory.castFactory('123');
+			let result = foo(Number(123));
+			expect(result).to.equal(123);
+		});
+
+		it('should convert "true" to true', () => {
+			let foo = CastFactory.castFactory('true');
+			let result = foo(Boolean(true));
+			expect(result).to.equal(true);
 		});
 
 		it('should throw an exception if unsupported format', () => {
