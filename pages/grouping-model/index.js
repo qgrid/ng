b8278230f52117245b51389e185ed5a841f72c71
qgrid <qgrid.team@gmail.com>
2017-08-28 16:20:12 +0300

    (deploy): CI

--- pages/grouping-model/index.js
+++ pages/grouping-model/index.js
@@ -1,9 +1,36 @@
-Controller.$inject = ['$http'];
-export default function Controller($http) {
+Controller.$inject = ['$http', 'qgrid'];
+export default function Controller($http, qgrid) {
 	const ctrl = this;
 	this.rows = [];
 
-	$http.get('data/people/100.json')
+	this.getGroupLabel = node => `Invoice Number: ${node.key}`;
+	this.getTotal = row => row.quantity * row.price;
+
+	this.rowStyle = (node, context) => {
+		if (node.type === 'group') {
+			context.class('group', {background: '#E3F2FD'});
+		}
+	};
+
+	this.rowOptions = {
+		trigger: 'click',
+		actions: [
+			new qgrid.Action(
+				new qgrid.Command({
+					canExecute: () => {
+						return true;
+					},
+					execute: () => {
+
+					}
+				}),
+				'Delete Row',
+				'delete'
+			)
+		]
+	};
+
+	$http.get('data/invoice/5.json')
 		.then(function (response) {
 			ctrl.rows = response.data;
 		});
