7f0cb77dd7349d71acf9e459a4523171882d4a5a
munkk <munkk@yandex.ru>
2017-08-29 19:05:05 +0300

    Update group.view.spec.js

--- src/core/group/group.view.spec.js
+++ src/core/group/group.view.spec.js
@@ -7,6 +7,20 @@ import {NavigationModel} from '../navigation/navigation.model';
 import {GroupView} from './group.view';
 
 describe('Group View', () => {
+	let node;
+
+	beforeEach('reset node', () => {
+		node = {
+			children: [1,2,3,4,5],
+			state: {
+				expand: true
+			},
+			level: 6,
+			key: 'name',
+			type: 'group'
+		};
+	});
+
 	window.ENV = {
 		PRODUCTION: false
 	};
@@ -27,14 +41,7 @@ describe('Group View', () => {
 		}]
 	];
 
-	let node = {
-		children: [1,2,3,4,5],
-		state: {
-			expand: true
-		},
-		level: 6,
-		key: 'name'
-	};
+
 
 	Model.register('group', GroupModel)
 		.register('view', ViewModel)
@@ -43,10 +50,29 @@ describe('Group View', () => {
 
 	model = new Model();
 	model.view({
-				columns: columns
-			});
+		columns: columns
+	});
+	model.navigation({
+		cell: 'CELL'
+	});
+
 	let commandManager = new CommandManager();
 	let groupView = new GroupView(model, commandManager);
+
+	describe('toggleStatus', () => {
+		it('execute() should toggle node.state from expand to collapse', () => {
+			let command = groupView.toggleStatus;
+			command.execute(node);
+			let result = groupView.status(node);
+			expect(result).to.equal('collapse');
+		});
+
+		it('should return true if canExecute', () => {
+			let command = groupView.toggleStatus;
+			let result = command.canExecute(node);
+			expect(result).to.equal(true);
+		});
+	});
 
 	describe('count', () => {
 		it('should return length of node.children or node.rows', () => {
@@ -65,7 +91,7 @@ describe('Group View', () => {
 	describe('offset', () => {
 		it('should return offset', () => {
 			let result = groupView.offset(node);
-			expect(result).to.equal(30);
+			expect(result).to.equal(30); // model.offset = 5 * node.level = 6 == 30
 		});
 	});
 
