81aa151f25c1e7c87eef99fef2d9b10864ebc9dd
munkk <munkk@yandex.ru>
2017-09-08 18:54:33 +0300

    Update unit.factory.d.ts

--- src/core/dom/selector/unit.factory.d.ts
+++ src/core/dom/selector/unit.factory.d.ts
@@ -1,9 +1,5 @@
-import {Range} from '../../infrastructure/range';
-
 export declare class UnitFactory {
-	constructor(rowRange: Range, columnRange: Range);
-	rowRange: Range;
-	columnRange: Range;
+	constructor();
 	cell(element: object, rowIndex: number, columnIndex: number);
 	row(element: object, rowIndex: number);
 }
