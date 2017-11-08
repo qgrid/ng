777e7e9d9796ced414cb0f2f43d15b8e4ee89fb5
klumba12 <dimitry.sitov@gmail.com>
2017-11-08 12:53:33 +0300

    close on save

--- src/plugins/persistence/persistence.panel.js
+++ src/plugins/persistence/persistence.panel.js
@@ -1,5 +1,18 @@
-export function PersistencePanelController() {
+import {Command} from '@grid/core/command';
+
+export function PersistencePanelController(mdPanelRef) {
 	const model = this.model;
 
-	this.items = model.persistence().items;
+	this.items = Array.from(model.persistence().items);
+
+	this.cancel = new Command({
+		execute: () => mdPanelRef.close()
+	});
+
+	this.submit = new Command({
+		execute: () => {
+			mdPanelRef.close();
+		},
+		canExecute: () => !!this.title
+	});
 }
