53a5162bfc8e906d91137fbc3fb1ad2df5ef0e41
Maxim Titov <maxim.titov@arcadia.spb.ru>
2017-11-16 14:56:56 +0300

    add storage ts definition

--- src/core/persistence/persistence.model.d.ts
+++ src/core/persistence/persistence.model.d.ts
@@ -1,7 +1,9 @@
 import {Command} from '../command/command';
+import {PersistenceStorage} from './persistence.storage';
 
 export declare class PersistenceModel {
 	id: string;
+	storage: PersistenceStorage;
 	load: Command;
 	remove: Command;
 	settings: object;
