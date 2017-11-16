dcf29d40169cc0d8f82147942e943f86dec4f1f0
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-11-16 18:02:13 +0300

    digest after dropdown click

--- src/plugins/backdrop/backdrop.js
+++ src/plugins/backdrop/backdrop.js
@@ -1,20 +1,27 @@
 import Component from '../../view/components/component';
 import {EventListener, EventManager} from '@grid/core/infrastructure';
+import {VIEW_CORE_NAME} from '@grid/view/definition';
 
 class Backdrop extends Component {
 	constructor($element, $document) {
 		super();
 
-		const document = $document[0];
-		const element = $element[0];
-		const listener = new EventListener(element, new EventManager(this));
+		this.document = $document[0];
+		this.element = $element[0];
+	}
+
+	onInit() {
+		const element = this.element;
+
+		const apply = this.$view.root.applyFactory(null, 'sync');
+		const listener = new EventListener(element, new EventManager(this, apply));
 
 		this.using(listener.on('click', e => {
 			e.stopPropagation();
 			element.remove();
 
 			if (this.propagate !== false) {
-				const target = document.elementFromPoint(e.clientX, e.clientY); // eslint-disable-line angular/document-service
+				const target = this.document.elementFromPoint(e.clientX, e.clientY); // eslint-disable-line angular/document-service
 				target.click();
 			}
 
@@ -34,6 +41,9 @@ export default {
 	templateUrl: 'qgrid.plugin.backdrop.tpl.html',
 	controller: Backdrop,
 	controllerAs: '$backdrop',
+	require: {
+		$view: `^^${VIEW_CORE_NAME}`
+	},
 	bindings: {
 		'onClose': '&',
 		'onKeyDown': '&',
--- src/plugins/cell-editor/cell.editor.js
+++ src/plugins/cell-editor/cell.editor.js
@@ -1,33 +1,9 @@
 import {EventListener, EventManager} from '@grid/core/infrastructure';
-import {VIEW_CORE_NAME} from '@grid/view/definition';
-import {Shortcut, ShortcutManager} from '@grid/core/shortcut';
 import Component from '../../view/components/component';
 
 class CellEditor extends Component {
-	constructor($element) {
+	constructor() {
 		super();
-
-		this.element = $element[0];
-	}
-
-	onInit() {
-		if (this.shortcut) {
-			// Create a local shortcut service to work directly with edit commands
-			const shortcut = new Shortcut(new ShortcutManager());
-			const $view = this.$view;
-			const edit = $view.edit.cell;
-			const element = this.element;
-
-			this.using(shortcut.register(edit.commandManager, edit.commands));
-
-			const listener = new EventListener(element, new EventManager(this, $view.root.applyFactory(null, 'sync')));
-			this.using(listener.on('keydown', e => {
-				if (shortcut.keyDown(e)) {
-					e.stopPropagation();
-					e.stopImmediatePropagation();
-				}
-			}));
-		}
 	}
 
 	close() {
@@ -35,13 +11,10 @@ class CellEditor extends Component {
 	}
 }
 
-CellEditor.$inject = ['$element'];
+CellEditor.$inject = [];
 
 export default {
 	transclude: true,
-	require: {
-		$view: `^^${VIEW_CORE_NAME}`
-	},
 	templateUrl: 'qgrid.plugin.cell-editor.tpl.html',
 	controller: CellEditor,
 	controllerAs: '$editor',
