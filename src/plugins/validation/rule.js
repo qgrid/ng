69031f0aedfa3113dbbdb8771682e182992ca4af
Ekaterina Tokareva <etokareva1990@gmail.com>
2017-11-17 19:04:48 +0300

    rule bindings, remove typo with ng-messages

--- src/plugins/validation/rule.js
+++ src/plugins/validation/rule.js
@@ -1,21 +1,35 @@
 import Component from '../../view/components/component';
 import {VALIDATION_NAME} from '../definition';
 
+const ruleBindings = {
+	length_between: '<? lengthBetween',
+	length_equal: '<? lengthEqual',
+	min_length: '<? minLength',
+	max_length: '<? maxLength',
+	one_of: '<? oneOf',
+	positive_integer: '@? positiveInteger',
+	required: '@?'
+};
+const bindings = {
+	for: '@',
+	key: '@'
+};
+Object.assign(bindings, ruleBindings);
+
 class Rule extends Component {
 	constructor() {
 		super(...arguments);
 	}
 
 	onInit() {
-		const predefinedRules = ['length_between', 'length_equal', 'max_length', 'min_length', 'one_of', 'positive_integer', 'required'];
+		const predefinedRules = Object.keys(ruleBindings);
 		const gridModel = this.validation.model;
 		const validation = gridModel.validation;
 		const rules = Array.from(validation().rules);
-		let rule = {
+		const rule = {
 			for: this.for,
 			key: this.key
 		};
-
 		for (let name of predefinedRules) {
 			if (this.hasOwnProperty(name)) {
 				rule[name] = this[name];
@@ -35,15 +49,5 @@ export default {
 		validation: `^^${VALIDATION_NAME}`
 	},
 	controllerAs: '$rule',
-	bindings: {
-		'for': '@',
-		'key': '@',
-		'length_between': '<? lengthBetween',
-		'length_equal': '<? lengthEqual',
-		'min_length': '<? minLength',
-		'max_length': '<? maxLength',
-		'one_of': '<? oneOf',
-		'positive_integer': '@? positiveInteger',
-		'required': '@?'
-	}
+	bindings: bindings
 };
