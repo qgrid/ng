e617e52ffd5fa44adb2f6deba070bc9e9a213ccd
Maxim Titov <mtitov92@gmail.com>
2017-12-01 17:25:02 +0300

    put position to core

--- src/view/directives/position.js
+++ src/view/directives/position.js
@@ -1,27 +1,20 @@
 import Directive from './directive';
 import {POSITION_NAME, GRID_NAME} from '@grid/view/definition';
-import {max} from '@grid/core/utility';
-import {EventListener, EventManager} from '@grid/core/infrastructure';
-import {jobLine} from '@grid/core/services';
-import {GRID_PREFIX} from '@grid/core/definition';
+import {PositionView} from '@grid/plugin/position/position.view';
 
 class Position extends Directive(POSITION_NAME, {root: `^?${GRID_NAME}`}) {
-	constructor($element, $attrs, $timeout, $window) {
+	constructor($element, $attrs, $window) {
 		super();
 
 		this.element = $element[0];
 		this.$attrs = $attrs;
-		this.$timeout = $timeout;
 		this.$window = $window;
 
-		const listener = new EventListener(this.$window, new EventManager(this));
-		const job = jobLine(400);
-
-		this.using(listener.on('resize', () => {
-			this.invalidate();
-			// In case if after window resize there can different animated layout changes
-			job(() => this.invalidate());
-		}));
+		this.position = new PositionView({
+			element: this.element,
+			window: this.$window
+		});
+		this.using(this.position.invalidateEvent.on(this.invalidate.bind(this)));
 	}
 
 	onInit() {
@@ -31,122 +24,12 @@ class Position extends Directive(POSITION_NAME, {root: `^?${GRID_NAME}`}) {
 	invalidate() {
 		if (this.root) {
 			const targetName = (this.$attrs[POSITION_NAME] || '').toLowerCase();
-			let node = this.element.parentNode;
-			while (node) {
-				if (node.nodeName.toLowerCase() === targetName) {
-					this.layout(node, this.element);
-					this.element.style.opacity = 1;
-					return;
-				}
-				node = node.parentNode;
-			}
+			this.position.invalidate(targetName);
 		}
-	}
-
-	layout(target, source) {
-		const {top, right, left, bottom} = target.getBoundingClientRect();
-		const {width, height} = source.getBoundingClientRect();
-		const br = this.boxRect();
-		const intersections = [];
-
-		intersections.push(
-			this.intersection(br, {
-				top: top,
-				right: left + width,
-				bottom: top + height,
-				left: left
-			}));
-
-		intersections.push(
-			this.intersection(br, {
-				top: top,
-				right: right,
-				bottom: top + height,
-				left: right - width
-			}));
-
-		intersections.push(
-			this.intersection(br, {
-				top: bottom - height,
-				right: left + width,
-				bottom: bottom,
-				left: left
-			}));
-
-		intersections.push(
-			this.intersection(br, {
-				top: bottom - height,
-				right: right,
-				bottom: bottom,
-				left: right - width
-			}));
-
-		const intersection = max(intersections, i => i.area);
-		const {left: l, top: t} = intersection.b;
-		const pos = this.fix({left: l - br.left, top: t - br.top, width, height});
-
-		source.style.left = pos.left + 'px';
-		source.style.top = pos.top + 'px';
-	}
-
-	intersection(a, b) {
-		const xo = Math.max(0, Math.min(a.right, b.right) - Math.max(a.left, b.left));
-		const yo = Math.max(0, Math.min(a.bottom, b.bottom) - Math.max(a.top, b.top));
-		const area = xo * yo;
-		return {area, a, b};
-	}
-
-	fix(rect) {
-		const wr = this.windowRect();
-		const br = this.boxRect();
-		const {width: vw, height: vh} = wr;
-		const vx = br.left - wr.left;
-		const vy = br.top - wr.top;
-		const {height: rh, width: rw} = rect;
-		const rx = rect.left + vx;
-		const ry = rect.top + vy;
-		const gtx1 = rx + rw > vw;
-		const ltx0 = rx < 0;
-		const gty1 = ry + rh > vh;
-		const lty0 = ry < 0;
-		const left = ltx0 || gtx1
-			? (vw - rw) / 2 - vx
-			: rect.left;
-		const top = lty0 || gty1
-			? (vh - rh) / 2 - vy
-			: rect.top;
-
-		return {left, top};
-	}
-
-	boxRect() {
-		let view = this.element;
-		const marker = `${GRID_PREFIX}-box`;
-		while (view) {
-			if (view.classList && view.classList.contains(marker)) {
-				return view.getBoundingClientRect();
-			}
-
-			view = view.parentNode;
-		}
-
-		return this.windowRect();
-	}
-
-	windowRect() {
-		const {innerHeight: h, innerWidth: w} = this.$window;
-		return {
-			top: 0,
-			left: 0,
-			bottom: h,
-			right: w,
-			height: h,
-			width: w
-		};
 	}
 }
 
-Position.$inject = ['$element', '$attrs', '$timeout', '$window'];
+Position.$inject = ['$element', '$attrs', '$window'];
 
 export default {
 	restrict: 'A',
