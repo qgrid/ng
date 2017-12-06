270ecf853c7c70c7baec6acece93102144daa1c6
klumba12 <dimitry.sitnov@gmail.com>
2017-12-06 05:06:38 -0500

    revert grid

--- core/grid/grid.model.js
+++ core/grid/grid.model.js
@@ -6,38 +6,5 @@ export class GridModel {
 		this.id = `q-grid-${guid()}`;
 		this.status = 'unbound'; //unbound | bound
 		this.title = '';
-		this.triggers = {
-			'data': {
-				'rows': PU.default, 
-				'columns': PU.column
-			},
-			'pagination': {
-				'current': PU.default, 
-				'size': PU.Default
-			},
-			'sort': {
-				'by': PU.default
-			},
-			'filter': {
-				'by': PU.default, 
-				'unit': PU.column
-			},
-			'group': {
-				'by': PU.default
-			},
-			'pivot': {
-				'by': PU.default				
-			},
-			'columnList': {
-				'index': PU.column
-			},
-			'row': {
-				'status': PU.rowDetails
-			},
-			'selection': {
-				'mode': PU.column,
-				'unit': PU.column   
-			}
-		};  	
 	}
 }
