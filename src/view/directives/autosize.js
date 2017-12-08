a3cba2b4b840ed3f97e12269016f35532e8e493d
valentin.korolev <valentin.korolev@arcadia.spb.ru>
2017-12-08 11:54:01 +0300

    Add cache to autosize directive. Refactor public methods

--- src/view/directives/autosize.js
+++ src/view/directives/autosize.js
@@ -9,58 +9,51 @@ class Autosize extends Directive(AUTOSIZE_NAME) {
 		this.$element = $element;
 		this.$timeout = $timeout;
 		this.$document = $document;
+		this.body = $document[0].body;
 		this.$attrs = $attrs;
+		this.cache = {};
+		this.referenceElement = null;
+		this.selector = null;
 	}
 
 	onInit() {
-		let $element = this.$element,
-			document = this.$document[0],
-			body = document.body,
-			selector = this.$attrs.autosize,
-			focusElement = null,
-			actualWidth = 0,
-			actualText = '';
+		this.selector = this.$attrs[AUTOSIZE_NAME];
+	}
 
-		function calculateWidth(text) {
-			let width = 0;
-			if (text) {
-				// TODO: a hack to prevent material flex layout inheritance
-				const container = document.createElement('div');
-				const test = container.appendChild(document.createElement('span'));
-				const referenceStyle = focusElement[0].style;
+	calculateWidth(text) {
+		let width = 0;
+		if (text) {
+			// TODO: a hack to prevent material flex layout inheritance
+			const container = document.createElement('div');
+			const test = container.appendChild(document.createElement('span'));
+			const referenceStyle = this.referenceElement[0].style;
 
-				test.textContent = text;
-				test.style.lineHeight = referenceStyle.lineHeight;
-				test.style.borderBox = referenceStyle.borderBox;
-				test.style.border = referenceStyle.border;
-				test.style.font = referenceStyle.font;
-				test.style.whiteSpace = 'pre';
-				test.style.visibility = 'hidden';
+			test.textContent = text;
+			test.style.lineHeight = referenceStyle.lineHeight;
+			test.style.borderBox = referenceStyle.borderBox;
+			test.style.border = referenceStyle.border;
+			test.style.font = referenceStyle.font;
+			test.style.whiteSpace = 'pre';
+			test.style.visibility = 'hidden';
 
-				body.append(container);
-				// TODO: cache widths be text ?
-				width = test.offsetWidth;
-				container.remove();
-			}
-
-			return width;
+			this.body.append(container);
+			width = test.offsetWidth;
+			container.remove();
 		}
 
-		this.width = function (text) {
-			if (!focusElement || !focusElement[0] || !focusElement.css('font')) {
-				focusElement = selector && $element.find(selector) || this.$element;
-				actualWidth = calculateWidth(text);
-				return actualWidth;
-			}
+		return width;
+	}
 
-			if (actualText === text) {
-				return actualWidth;
-			}
+	width(text) {
+		if (!this.referenceElement || !this.referenceElement.length) {
+			this.referenceElement = this.selector && this.$element.find(this.selector) || this.$element;
+		}
 
-			actualText = text;
-			actualWidth = calculateWidth(text);
-			return actualWidth;
-		};
+		if (!this.cache.hasOwnProperty(text)) {
+			this.cache[text] = this.calculateWidth(text);
+		}
+
+		return this.cache[text];
 	}
 }
 
