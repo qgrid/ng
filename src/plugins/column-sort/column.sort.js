08ac353423c672ddc1c1d0736a15a941cd9e6103
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-12-15 18:13:04 +0300

    get rid of ng-class

--- src/plugins/column-sort/column.sort.js
+++ src/plugins/column-sort/column.sort.js
@@ -1,17 +1,31 @@
 import Component from '../../view/components/component';
 import {VIEW_CORE_NAME} from '@grid/view/definition';
-import {Event, EventListener, EventManager} from '@grid/core/infrastructure';
+import {GRID_PREFIX} from '@grid/core/definition';
 
 class ColumnSort extends Component {
-	constructor() {
+	constructor($element) {
 		super();
+
+		this.element = $element[0];
 	}
 
 	onInit() {
+		const view = this.view;
+		const model = view.model;
+		model.sortChanged.watch(e => {
+			if (e.hasChanges('by')) {
+				if (view.sort.order(this.column) >= 0) {
+					this.element.classList.add(`${GRID_PREFIX}-active`);
+				}
+				else {
+					this.element.classList.remove(`${GRID_PREFIX}-active`);
+				}
+			}
+		});
 	}
 }
 
-ColumnSort.$inject = ['$scope'];
+ColumnSort.$inject = ['$element'];
 
 export default {
 	transclude: true,
