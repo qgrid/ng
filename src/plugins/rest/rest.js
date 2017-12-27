60c9e528f76fb7de180208584c46206ffa574980
valentin.korolev <valentin.korolev@arcadia.spb.ru>
2017-12-27 15:18:04 +0300

    Include rest plugin

--- src/plugins/rest/rest.js
+++ src/plugins/rest/rest.js
@@ -1,5 +1,4 @@
 import PluginComponent from '../plugin.component';
-import {AppError} from '@grid/core/infrastructure';
 import {Rest} from '@grid/plugin/rest/rest';
 
 const Plugin = PluginComponent('data-manipulation', {inject: ['$http']});
@@ -10,27 +9,11 @@ class RestPlugin extends Plugin {
 	}
 
 	onInit() {
-		if (!this.url) {
-			throw new AppError('rest', 'REST endpoint is required');
-		}
-
-		this.method = (this.method || 'get').toLowerCase();
-
-		let fetch;
-
-		if (this.method === 'get') {
-			fetch = params => this.$http.get(this.url, params);
-		} else if (this.method === 'post') {
-			fetch = params => this.$http.post(this.url, params);
-		} else {
-			throw new AppError('rest', `${this.method} is illegal. Use GET or POST`);
-		}
-
 		new Rest(this.model, {
 			url: this.url,
 			method: this.method,
-			get: (url, params) => this.$http.get(url, params),
-			post: (url, params) => this.$http.post(url, params),
+			get: (url, params) => this.$http.get(url, {params}),
+			post: (url, data) => this.$http.post(url, {data}),
 			serialize: this.serialize
 		});
 	}
@@ -42,7 +25,6 @@ export default RestPlugin.component({
 	bindings: {
 		url: '<',
 		method: '<',
-		fetch: '<',
 		serialize: '<'
 	}
 });
