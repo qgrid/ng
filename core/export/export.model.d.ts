d612ebdbec0876f668bc4d7756f2b46f52c8ea73
Ekaterina Tokareva <Tokaree@labcorp.com>
2018-01-10 19:38:08 +0300

    export and import models init, move dtses

--- core/export/export.model.d.ts
+++ core/export/export.model.d.ts
@@ -1,6 +1,12 @@
 import {Resource} from '../resource/resource';
 
+/**
+ * A class to handle export settings.
+ *
+ */
+
 export declare class ExportModel {
 	constructor();
+
 	resource: Resource;
 }
--- core/import/import.model.d.ts
+++ core/import/import.model.d.ts
@@ -1,6 +1,12 @@
 import {Resource} from '../resource/resource';
 
+/**
+ * A class to handle import settings.
+ *
+ */
+
 export declare class ImportModel {
 	constructor();
+
 	resource: Resource;
 }
