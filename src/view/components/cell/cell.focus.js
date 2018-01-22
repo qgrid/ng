670491b37a3d64dbbc1c74dbfa3ac04d4f18298f
klumba12 <dimitry.sitnov@gmail.com>
2018-01-22 21:23:02 +0300

    add styles to cell.focus

--- src/view/components/cell/cell.focus.js
+++ src/view/components/cell/cell.focus.js
@@ -1,20 +1,36 @@
 import Component from '../component';
-import {GRID_NAME} from '@grid/view/definition';
+import { GRID_NAME } from '@grid/view/definition';
 
 class CellFocus extends Component {
-	constructor() {
-		super();
-	}
+    constructor($element) {
+        super();
 
-	onInit() {
-	}
+        this.element = $element[0];
+    }
+
+    onInit() {
+        this.root.model.focusChanged.on(e => {
+            if (e.hasChanges('rowIndex') || e.hasChanges('columnIndex')) {
+                const table = this.root.table;
+                const cell = table.body.cell(e.state.rowIndex, e.state.columnIndex);
+                const model = cell.model();
+                if (model) {
+                    const rect = cell.rect();
+                    this.element.style.top = rect.top + 'px';
+                    this.element.style.left = rect.left + 'px';
+                    this.element.style.width = rect.width + 'px';
+                    this.element.style.height = rect.height + 'px';
+                }
+            }
+        });
+    }
 }
 
-CellFocus.$inject = [];
+CellFocus.$inject = ['$element'];
 
 export default {
-	require: {
-		root: `^^${GRID_NAME}`
-	},
-	controller: CellFocus
+    require: {
+        root: `^^${GRID_NAME}`
+    },
+    controller: CellFocus
 };
