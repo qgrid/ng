6c32bcdfb28031400ce9e15174c36d9f6e715af2
klumba12 <dimitry.sitnov@gmail.com>
2018-01-23 10:50:30 +0300

    add defer

--- src/view/components/cell/cell.focus.js
+++ src/view/components/cell/cell.focus.js
@@ -1,42 +1,47 @@
 import Component from '../component';
 import { GRID_NAME } from '@grid/view/definition';
+import { jobLine } from '@grid/core/services/job.line';
 
 class CellFocus extends Component {
-    constructor($element) {
-        super();
+	constructor($element) {
+		super();
 
-        this.element = $element[0];
-    }
+		this.element = $element[0];
+		this.job = jobLine(200);
+	}
 
-    onInit() {
-        const model = this.root.model;
-        const table = this.root.table;
-        const element = this.element;
-        model.focusChanged.on(e => {
-            element.classList.add('q-grid-active');
-            if (e.hasChanges('rowIndex') || e.hasChanges('columnIndex')) {
-                const cell = table.body.cell(e.state.rowIndex, e.state.columnIndex);
-                cell.addClass('q-grid-animate');
-                const target = cell.element;
-                const scrollState = model.scroll();
-                element.style.top = (target.offsetTop - scrollState.top) + 'px';
-                element.style.left = (target.offsetLeft - scrollState.left) + 'px';
-                element.style.width = target.clientWidth + 'px';
-                element.style.height = target.clientHeight + 'px';
-                setTimeout(() => {
-                    element.classList.remove('q-grid-active');
-                    cell.removeClass('q-grid-animate');
-                }, 200);
-            }
-        });
-    }
+	onInit() {
+		const model = this.root.model;
+		const table = this.root.table;
+		const element = this.element;
+		model.focusChanged.on(e => {
+			if (e.hasChanges('rowIndex') || e.hasChanges('columnIndex')) {
+				element.classList.add('q-grid-active');
+	
+				const cell = table.body.cell(e.state.rowIndex, e.state.columnIndex);
+				cell.addClass('q-grid-animate');
+				const target = cell.element;
+				const scrollState = model.scroll();
+				element.style.top = (target.offsetTop - scrollState.top) + 'px';
+				element.style.left = (target.offsetLeft - scrollState.left) + 'px';
+				element.style.width = target.offsetWidth + 'px';
+				element.style.height = target.offsetHeight + 'px';
+				this.job(() => {
+					element.classList.remove('q-grid-active');
+					cell.removeClass('q-grid-animate');
+				}).catch(() => {
+					cell.removeClass('q-grid-animate');
+				});
+			}
+		});
+	}
 }
 
 CellFocus.$inject = ['$element'];
 
 export default {
-    require: {
-        root: `^^${GRID_NAME}`
-    },
-    controller: CellFocus
+	require: {
+		root: `^^${GRID_NAME}`
+	},
+	controller: CellFocus
 };
