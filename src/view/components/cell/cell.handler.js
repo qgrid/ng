95d7c4978f2b249ac47cb657aeb543067d83e735
klumba12 <dimitry.sitnov@gmail.com>
2018-02-07 13:11:14 +0300

    correct animation for cell handler

--- src/view/components/cell/cell.handler.js
+++ src/view/components/cell/cell.handler.js
@@ -14,26 +14,49 @@ class CellHandler extends Component {
 		const model = this.root.model;
 		const table = this.root.table;
 		const element = this.element;
-		model.focusChanged.on(e => {
-			if (e.hasChanges('rowIndex') || e.hasChanges('columnIndex')) {
-				const cell = table.body.cell(e.state.rowIndex, e.state.columnIndex);
-				const cellModel = cell.model();
-				if (cellModel) {
-					element.classList.add('q-grid-active');
-					cell.addClass('q-grid-animate');
-					const target = cell.element;
+
+		// When navigate first or when animation wasn't applied we need to omit
+		// next navigation event to make handler to correct position.
+		let isValid = false;
+		model.navigationChanged.watch(e => {
+			if (e.hasChanges('cell')) {
+				const domCell = table.body.cell(e.state.rowIndex, e.state.columnIndex);
+				const cell = e.state.cell;
+				if (cell) {
+					const oldColumn = (e.changes.cell.oldValue || {}).column || {};
+					const newColumn = (e.changes.cell.newValue || {}).column || {};
+
+					// Do not apply animation for columns that have viewWidth assigned
+					// because it can be animated too.
+					const shouldAnimate = oldColumn.key === newColumn.key || !(oldColumn.viewWidth || newColumn.viewWidth);
+					if (!shouldAnimate) {
+						isValid = false;
+						return;
+					}
+
+					if (isValid) {
+						element.classList.add('q-grid-active');
+						domCell.addClass('q-grid-animate');
+					}
+
+					const target = domCell.element;
 					const scrollState = model.scroll();
 
 					element.style.top = (target.offsetTop - scrollState.top) + 'px';
-					element.style.left = (target.offsetLeft - (cellModel.column.pin ? 0 : scrollState.left)) + 'px';
+					element.style.left = (target.offsetLeft - (cell.column.pin ? 0 : scrollState.left)) + 'px';
 					element.style.width = target.offsetWidth + 'px';
 					element.style.height = target.offsetHeight + 'px';
-					this.job(() => {
-						element.classList.remove('q-grid-active');
-						cell.removeClass('q-grid-animate');
-					}).catch(() => {
-						cell.removeClass('q-grid-animate');
-					});
+
+					if (isValid) {
+						this.job(() => {
+							element.classList.remove('q-grid-active');
+							domCell.removeClass('q-grid-animate');
+						}).catch(() => {
+							domCell.removeClass('q-grid-animate');
+						});
+					}
+
+					isValid = true;
 				}
 			}
 		});
