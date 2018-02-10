334ffe485705e2410e5a858f08880bf3add5ecbc
klumba12 <dimitry.sitnov@gmail.com>
2018-02-10 07:07:54 +0300

    layer to new responsibility

--- core/dom/fake/layer.js
+++ core/dom/fake/layer.js
@@ -2,9 +2,6 @@ export class FakeLayer {
 	constructor() {
 	}
 
-	resource() {
-	}
-
 	destroy() {
 	}
 }
--- src/view/services/layer.factory.js
+++ src/view/services/layer.factory.js
@@ -1,16 +1,23 @@
-import {AppError} from '@grid/core/infrastructure';
-import {VIEW_CORE_NAME} from '../definition';
-import {Layer} from './layer';
-import {GRID_PREFIX} from '@grid/core/definition';
+import { AppError } from '@grid/core/infrastructure';
+import { VIEW_CORE_NAME } from '../definition';
+import { Layer } from './layer';
+import { FakeLayer } from '@grid/core/dom/fake/layer';
+import { GRID_PREFIX } from '@grid/core/definition';
 import * as css from '@grid/core/services/css';
 
 export class LayerFactory {
-	constructor(markup, template) {
+	constructor(markup, template, model) {
 		this.markup = markup;
 		this.template = template;
+		this.model = model;
 	}
 
 	create(name) {
+		const resource = this.model.layer().resource;
+		if (!resource.data.hasOwnProperty(name)) {
+			return new FakeLayer();
+		}
+
 		const markup = this.markup;
 		const node = markup.document.createElement('div');
 		node.classList.add(css.escapeAttr(name));
@@ -26,6 +33,12 @@ export class LayerFactory {
 			throw new AppError('layer.factory', 'Controller scope for box is not found');
 		}
 
-		return new Layer(ctrl.$scope, node, this.template);
+		return new Layer(
+			ctrl.$scope,
+			node,
+			this.template,
+			name,
+			resource
+		);
 	}
 }
