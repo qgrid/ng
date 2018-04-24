eef9e54d2432029510481fc0a9f132d2b5551c6f
Maxim Titov <mtitov92@gmail.com>
2018-04-24 18:42:37 +0300

    sync latest changes for persistence plugin with ng2-qgrid

--- demo/pages/persistence/index.js
+++ demo/pages/persistence/index.js
@@ -1,8 +1,30 @@
-Controller.$inject = ['$http'];
-export default function Controller($http) {
+Controller.$inject = ['$http', 'qgrid'];
+export default function Controller($http, qgrid) {
 	const ctrl = this;
 
 	this.rows = [];
+
+	this.gridModel = qgrid.model();
+	const persistence = this.gridModel.persistence;
+	const storage = persistence().storage;
+	persistence({
+		storage: {
+			getItem: id => {
+				return new Promise(resolve => {
+					storage.getItem(id)
+						.then(myPresets => {
+							$http.get('data/presets.json')
+								.then(response => {
+									const presets = response.data;
+									const allPresets = presets.concat(myPresets || []);
+									resolve(allPresets);
+								});
+						});
+				});
+			},
+			setItem: storage.setItem.bind(storage)
+		}
+	});
 
 	$http.get('data/people/100.json')
 		.then(function (response) {
