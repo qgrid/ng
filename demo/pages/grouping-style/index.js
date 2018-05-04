756e1a88fc1b0a925b5990f1d137b52fcad4e072
klumba12 <dimitry.sitnov@gmail.com>
2018-05-04 13:51:29 +0300

    added example with group styling

---
+++ demo/pages/grouping-style/index.js
@@ -0,0 +1,47 @@
+Controller.$inject = ['$http', 'qgrid'];
+
+export default function Controller($http, qgrid) {
+	const model = qgrid.model();
+	this.gridModel = model;
+
+	model.style({
+		cell: function (node, column, context) {
+			if (column.key === 'gender') {
+				if (node.type === 'row') {
+					context.class('female', {
+						color: 'red'
+					});
+				}
+			}
+		}
+	});
+
+	this.getGenderLabel = node => {
+		const rows = model.data().rows;
+		switch (node.type) {
+			case 'group': {
+				const stat = node
+					.rows
+					.reduce(function (memo, index) {
+						const row = rows[index];
+						memo[row.gender] = memo[row.gender] + 1;
+						return memo;
+					}, {
+						male: 0,
+						female: 0
+					});
+
+				return 'male: ' + stat.male + ' female: ' + stat.female;
+			}
+			default: {
+				const index = node.rows[0];
+				return rows[index].gender;
+			}
+		}
+	};
+
+	$http.get('data/people/10000.json')
+		.then(response => model.data({
+			rows: response.data
+		}));
+}
